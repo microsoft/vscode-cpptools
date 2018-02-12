4c371c926d3d5b524fa0d8f2b5bb739778d31370
Bob Brown <bobbrow@microsoft.com>
2018-02-12 14:04:34 -0800

    add a way to speed up subsequent launches of the extension.

---
+++ Extension/src/Support/update.js
@@ -0,0 +1,15 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All Rights Reserved.
+ * See 'LICENSE' in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+/**
+ * This file is used during local debugging of the extension and should not be referenced by any
+ * other source files.
+ */
+
+const fs = require("fs");
+if (fs.existsSync('./node_modules')) {
+    throw "no need npm install";
+}
