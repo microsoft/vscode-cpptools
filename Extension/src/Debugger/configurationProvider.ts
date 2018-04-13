48ff2d609e73e2a0dc00aeefdea0fa6bb2a29845
Andrew Wang <waan@microsoft.com>
2018-04-13 14:25:44 -0700

    Fix tslint issue

--- Extension/src/Debugger/configurationProvider.ts
+++ Extension/src/Debugger/configurationProvider.ts
@@ -4,7 +4,6 @@
  * ------------------------------------------------------------------------------------------ */
 
 import * as debugUtils from './utils';
-import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 import * as process from 'process';
