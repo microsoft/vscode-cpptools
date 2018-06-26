35288aad1778e4b7468d752a7a58e2b2e225efdf
Bob Brown <bobbrow@users.noreply.github.com>
2018-06-26 12:35:58 -0700

    start sending installation failure telemetry again (#2189)

--- Extension/src/installationInformation.ts
+++ Extension/src/installationInformation.ts
@@ -3,7 +3,7 @@
  * See 'LICENSE' in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-export class InstallationInformation  {
+export class InstallationInformation {
     stage: string;
     hasError: boolean;
     telemetryProperties: { [key: string]: string };
@@ -14,19 +14,15 @@ export class InstallationInformation  {
     }
 }
 
-let installBlob: InstallationInformation ;
+let installBlob: InstallationInformation;
 
-export function initializeInstallationInformation(): void {
-    installBlob = new InstallationInformation ();
-}
-
-export function getInstallationInformationInstance(): InstallationInformation  {
+export function getInstallationInformation(): InstallationInformation {
+    if (!installBlob) {
+        installBlob = new InstallationInformation();
+    }
     return installBlob;
 }
 
 export function setInstallationStage(stage: string): void {
-    if (!installBlob) {
-        initializeInstallationInformation();
-    }
-    installBlob.stage = stage;
+    getInstallationInformation().stage = stage;
 }
