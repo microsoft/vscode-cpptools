8736bb8bed6ac1ed872cc338eff7207c42cc65d8
John Patterson <john@JohnPPatterson.com>
2018-07-23 21:59:31 -0700

    tests existing tag resolution functionality

--- Extension/test/unitTests/common.test.ts
+++ Extension/test/unitTests/common.test.ts
@@ -1,16 +1,79 @@
 import * as assert from "assert";
-import * as vscode from "vscode";
-import { resolveVariables } from "../../src/common";
+import {  resolveVariables } from "../../src/common";
 
 suite("Common Utility validation", () => {
     suite("resolveVariables", () => {
+        const success = "success";
+        const home = process.env.HOME;
+
         test("raw input", () => {
             const input = "test";
             assert.equal(resolveVariables(input, {}), input);
         });
 
+        test("raw input with tilde", () => {
+            const input = "~/test";
+            assert.equal(resolveVariables(input, {}), `${home}/test`)
+        });
+
+        test("env input with tilde", () => {
+            const input = "${path}/test";
+            const actual = resolveVariables(input, {
+                path: home
+            }); 
+            assert.equal(actual, `${home}/test`)
+        });
+
+        test("solo env input resulting in array", () => {
+            const input = "${test}";
+            const actual = resolveVariables(input, {
+                test: ["foo", "bar"]
+            }); 
+            assert.equal(actual, "foo;bar")
+        });
+
+        test("mixed raw and env input resulting in array", () => {
+            const input = "baz${test}";
+            const actual = resolveVariables(input, {
+                test: ["foo", "bar"]
+            }); 
+            assert.equal(actual, input)
+        });
+
+        test("solo env input not in env config finds process env", () => {
+            const processKey = `cpptoolstests_${Date.now()}`;
+            const input = "foo${" + processKey + "}";
+            let actual: string;
+            try {
+                process.env[processKey] = "bar";
+                actual = resolveVariables(input, {});
+            } finally {
+                delete process.env[processKey];
+            }
+            assert.equal(actual, "foobar");
+        });
+
         test("env input", () => {
             shouldSuccessfullyLookupInEnv("${test}", "test");
+        });
+
+        test("env input mixed with plain text", () => {
+            const input = "${test}bar";
+            const env = {
+                test: "foo"
+            };
+            const result = resolveVariables(input, env);
+            assert.equal(result, "foobar");
+        });
+
+        test("env input with two variables", () => {
+            const input = "f${a}${b}r";
+            const env = {
+                a: "oo",
+                b: "ba"
+            };
+            const result = resolveVariables(input, env);
+            assert.equal(result, "foobar");
         });
 
         test("env input not in env", () => {
@@ -38,8 +101,8 @@ suite("Common Utility validation", () => {
             shouldSuccessfullyLookupInEnv("${env:Root}", "Root");
         });
 
+
         const shouldSuccessfullyLookupInEnv = (input: string, expectedResolvedKey: string) => {
-            const success = "success";
             const env = {};
             env[expectedResolvedKey] = success;
             const result = resolveVariables(input, env);
