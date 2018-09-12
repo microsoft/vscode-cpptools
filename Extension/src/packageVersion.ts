97809086c9af0e0c11764f925c34226d2910cbdc
Griffin Downs <grdowns@microsoft.com>
2018-09-12 10:29:16 -0700

    Modify version comparison (untested)

--- Extension/src/packageVersion.ts
+++ Extension/src/packageVersion.ts
@@ -11,6 +11,7 @@ export class PackageVersion {
     minor: number;
     patch: number;
     suffix?: string;
+    suffixVersion: number;
     isValid: boolean;
 
     constructor(versionStr: string) {
@@ -25,6 +26,7 @@ export class PackageVersion {
         this.minor = parseInt(tokens[1]);
         this.patch = parseInt(tokens[2]);
         this.suffix = tokens[3];
+        this.suffixVersion = tokens.length >= 5 ? parseInt(tokens[4]) : 0;
 
         if (this.major === undefined || this.minor === undefined || this.patch === undefined) {
             this.isValid = false;
@@ -36,21 +38,33 @@ export class PackageVersion {
     }
 
     public isGreaterThan(other: PackageVersion): boolean {
+        if (!this.isValid || !other.isValid) {
+            return false;
+        }
         // PackageVersions cannot be compared if either have a suffix that is not 'insiders'
-        if ((this.suffix && this.suffix !== 'insiders') || (other.suffix && other.suffix !== 'insiders')) {
+        if ((this.suffix && !this.suffix.startsWith('insiders')) || (other.suffix && !other.suffix.startsWith('insiders'))) {
             return false;
         }
 
-        let diff: number = other.major - this.major;
+        let diff: number = this.major - other.major;
         if (diff) {
             return diff > 0;
-        } else if (diff = other.minor - this.minor) {
+        } else if (diff = this.minor - other.minor) {
             return diff > 0;
-        } else if (diff = other.patch - this.patch) {
+        } else if (diff = this.patch - other.patch) {
             return diff > 0;
-        } else if (!other.suffix && this.suffix === 'insiders') {
-            return true;
+        } else {
+            if (this.suffix) {
+                if (other.suffix) {
+                    return this.suffixVersion > other.suffixVersion;
+                } else {
+                    return false;
+                }
+            } else if (other.suffix) {
+                return true;
+            } else {
+                return false;
+            }
         }
-        return false;
     }
 }
