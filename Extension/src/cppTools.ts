2f931d930d6ba3ec37a2fb7e082d1d8d353cd078
Bob Brown <bobbrow@users.noreply.github.com>
2018-09-18 16:33:08 -0700

    Implement provider API v2 (#2531)
    * Implement provider API v2

--- Extension/src/cppTools.ts
+++ Extension/src/cppTools.ts
@@ -7,38 +7,99 @@
 import { CustomConfigurationProvider, Version } from 'vscode-cpptools';
 import { CppToolsTestApi, CppToolsTestHook } from 'vscode-cpptools/out/testApi';
 import { CustomConfigurationProvider1, getCustomConfigProviders, CustomConfigurationProviderCollection } from './LanguageServer/customProviders';
+import { getOutputChannel } from './logger';
 import * as LanguageServer from './LanguageServer/extension';
 import * as test from './testHook';
 
 export class CppTools implements CppToolsTestApi {
     private version: Version;
     private providers: CustomConfigurationProvider1[] = [];
+    private timers = new Map<string, NodeJS.Timer>();
 
     constructor(version: Version) {
+        if (version > Version.latest) {
+            console.warn(`version ${version} is not supported by this version of cpptools`);
+            console.warn(`  using ${Version.latest} instead`);
+            version = Version.latest;
+        }
         this.version = version;
     }
 
-    registerCustomConfigurationProvider(provider: CustomConfigurationProvider): void {
-        let providers: CustomConfigurationProviderCollection = getCustomConfigProviders();
-        if (providers.add(provider, this.version)) {
-            let added: CustomConfigurationProvider1 = providers.get(provider);
-            this.providers.push(added);
-            LanguageServer.getClients().forEach(client => client.onRegisterCustomConfigurationProvider(added));
+    private addNotifyReadyTimer(provider: CustomConfigurationProvider1): void {
+        if (this.version >= Version.v2) {
+            const timeout: number = 30;
+            let timer: NodeJS.Timer = setTimeout(() => {
+                console.warn(`registered provider ${provider.extensionId} did not call 'notifyReady' within ${timeout} seconds`);
+            }, timeout * 1000);
+            this.timers.set(provider.extensionId, timer);
         }
     }
 
-    didChangeCustomConfiguration(provider: CustomConfigurationProvider): void {
+    private removeNotifyReadyTimer(provider: CustomConfigurationProvider1): void {
+        if (this.version >= Version.v2) {
+            let timer: NodeJS.Timer = this.timers.get(provider.extensionId);
+            if (timer) {
+                this.timers.delete(provider.extensionId);
+                clearTimeout(timer);
+            }
+        }
+    }
+
+    public getVersion(): Version {
+        return this.version;
+    }
+
+    public registerCustomConfigurationProvider(provider: CustomConfigurationProvider): void {
+        let providers: CustomConfigurationProviderCollection = getCustomConfigProviders();
+        if (providers.add(provider, this.version)) {
+            let added: CustomConfigurationProvider1 = providers.get(provider);
+            getOutputChannel().appendLine(`Custom configuration provider '${added.name}' registered`);
+            this.providers.push(added);
+            LanguageServer.getClients().forEach(client => client.onRegisterCustomConfigurationProvider(added));
+            this.addNotifyReadyTimer(added);
+        }
+    }
+
+    public notifyReady(provider: CustomConfigurationProvider): void {
         let providers: CustomConfigurationProviderCollection = getCustomConfigProviders();
         let p: CustomConfigurationProvider1 = providers.get(provider);
 
         if (p) {
+            this.removeNotifyReadyTimer(p);
+            p.isReady = true;
+            LanguageServer.getClients().forEach(client => {
+                client.updateCustomConfigurations(p);
+                client.updateCustomBrowseConfiguration(p);
+            });
+        } else {
+            console.assert(false, "provider should be registered before signaling it's ready to provide configurations");
+        }
+    }
+
+    public didChangeCustomConfiguration(provider: CustomConfigurationProvider): void {
+        let providers: CustomConfigurationProviderCollection = getCustomConfigProviders();
+        let p: CustomConfigurationProvider1 = providers.get(provider);
+
+        if (p) {
+            console.assert(p.isReady, "didChangeCustomConfiguration was invoked before notifyReady");
             LanguageServer.getClients().forEach(client => client.updateCustomConfigurations(p));
         } else {
             console.assert(false, "provider should be registered before sending config change messages");
         }
     }
 
-    dispose(): void {
+    public didChangeCustomBrowseConfiguration(provider: CustomConfigurationProvider): void {
+        let providers: CustomConfigurationProviderCollection = getCustomConfigProviders();
+        let p: CustomConfigurationProvider1 = providers.get(provider);
+
+        if (p) {
+            LanguageServer.getClients().forEach(client => client.updateCustomBrowseConfiguration(p));
+        } else {
+            console.assert(false, "provider should be registered before sending config change messages");
+        }
+    }
+
+    public dispose(): void {
         this.providers.forEach(provider => {
             getCustomConfigProviders().remove(provider);
             provider.dispose();
@@ -46,7 +107,7 @@ export class CppTools implements CppToolsTestApi {
         this.providers = [];
     }
 
-    getTestHook(): CppToolsTestHook {
+    public getTestHook(): CppToolsTestHook {
         return test.getTestHook();
     }
 }
