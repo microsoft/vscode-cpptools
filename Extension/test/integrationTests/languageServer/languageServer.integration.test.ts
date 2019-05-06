1ff57f179f8711458c5ab9ee39ec634b7f48fd5a
Sean McManus <seanmcm@microsoft.com>
2019-05-06 15:43:33 -0700

    Reduce timeout.

--- Extension/test/integrationTests/languageServer/languageServer.integration.test.ts
+++ Extension/test/integrationTests/languageServer/languageServer.integration.test.ts
@@ -13,7 +13,7 @@ import * as config from '../../../src/LanguageServer/configurations';
 import { getActiveClient } from '../../../src/LanguageServer/extension';
 import { initializeTemporaryCommandRegistrar } from '../../../src/commands';
 
-const defaultTimeout: number = 60000;
+const defaultTimeout: number = 10000;
 
 suite("multiline comment setting tests", function(): void {
     suiteSetup(async function(): Promise<void> { 
