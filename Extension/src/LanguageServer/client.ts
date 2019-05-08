ff5ab0ea879fa9f6f504a46e055e7635e61a5d0b
Sean McManus <seanmcm@microsoft.com>
2019-05-08 12:16:41 -0700

    Add telemetry for missing binaries.

--- Extension/src/LanguageServer/client.ts
+++ Extension/src/LanguageServer/client.ts
@@ -402,6 +402,7 @@ class DefaultClient implements Client {
         let serverModule: string = getLanguageServerFileName();
         let exeExists: boolean = fs.existsSync(serverModule);
         if (!exeExists) {
+            telemetry.logLanguageServerEvent("missingBinary");
             throw String('Missing binary at ' + serverModule);
         }
         let serverName: string = this.getName(workspaceFolder);
