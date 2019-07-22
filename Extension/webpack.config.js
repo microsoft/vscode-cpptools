dca7cef08d147c8165c1367b79bbb6fe1acd6dbb
Colen Garoutte-Carson <cogarout@microsoft.com>
2019-07-22 16:47:26 -0700

    some cleanup

--- Extension/webpack.config.js
+++ Extension/webpack.config.js
@@ -12,6 +12,7 @@ const path = require('path');
 /**@type {import('webpack').Configuration}*/
 const config = {
     target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+
     entry: './src/main.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
     output: { // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
         path: path.resolve(__dirname, 'dist'),
