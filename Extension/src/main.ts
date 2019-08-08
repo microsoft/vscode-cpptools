ba2da8d98ca1f3aa644642250a5d2778c4fd9a07
Griffin Downs <35574547+grdowns@users.noreply.github.com>
2019-08-08 09:28:18 -0700

    Vcpkg integration (#3791)

--- Extension/src/main.ts
+++ Extension/src/main.ts
@@ -369,6 +369,8 @@ function rewriteManifest(): Promise<void> {
         "onCommand:C_Cpp.TakeSurvey",
         "onCommand:C_Cpp.LogDiagnostics",
         "onCommand:C_Cpp.RescanWorkspace",
+        "onCommand:C_Cpp.VcpkgClipboardInstallSuggested",
+        "onCommand:C_Cpp.VcpkgClipboardOnlineHelpSuggested",
         "onDebug",
         "workspaceContains:/.vscode/c_cpp_properties.json"
     ];
