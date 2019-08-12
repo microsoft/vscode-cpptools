57f9ee58f1948690ce5a31f45fba4b57946af915
Colen Garoutte-Carson <cogarout@microsoft.com>
2019-08-12 14:37:28 -0700

    Update import_edge_strings.js to accept Localize repo path instead of VS repo path

--- Extension/import_edge_strings.js
+++ Extension/import_edge_strings.js
@@ -4,21 +4,19 @@ const fs = require('fs');
 const path = require('path');
 const parseString = require('xml2js').parseString;
 
-let vsRepoPath = process.argv[2];
+let localizeRepoPath = process.argv[2];
 let cpptoolsRepoPath = process.argv[3];
 
-if (!vsRepoPath || !cpptoolsRepoPath) {
-    console.error(`ERROR: Usage: ${path.parse(process.argv[0]).base} ${path.parse(process.argv[1]).base} <VS repo path> <vscode-cpptools repo path>`);
+if (!localizeRepoPath || !cpptoolsRepoPath) {
+    console.error(`ERROR: Usage: ${path.parse(process.argv[0]).base} ${path.parse(process.argv[1]).base} <Localize repo path> <vscode-cpptools repo path>`);
     return;
 }
 
-console.log("Importing EDGE strings from VS repo: " + vsRepoPath);
+console.log("Importing EDGE strings from Localize repo: " + localizeRepoPath);
 console.log("Writing to cpptols repo: " + cpptoolsRepoPath);
 
-let locPath = path.join(vsRepoPath, "auxsrc/Localize");
-
-if (!fs.existsSync(path.join(locPath, ".git"))) {
-    console.error("ERROR: Localize repo submodule is not initialized in VS repo");
+if (!fs.existsSync(path.join(localizeRepoPath, ".git"))) {
+    console.error("ERROR: Localize repo submodule is not initialized in Localize repo");
     return;
 }
 
@@ -40,9 +38,9 @@ const languages = [
     { id: "pl", folderName: "plk" }
 ];
 
-var locFolderNames = fs.readdirSync(locPath).filter(f => fs.lstatSync(path.join(locPath, f)).isDirectory());
+var locFolderNames = fs.readdirSync(localizeRepoPath).filter(f => fs.lstatSync(path.join(localizeRepoPath, f)).isDirectory());
 locFolderNames.forEach((locFolderName) => {
-    let lclPath = path.join(locPath, locFolderName, "vc/vc/cpfeui.dll.lcl");
+    let lclPath = path.join(localizeRepoPath, locFolderName, "vc/vc/cpfeui.dll.lcl");
     let languageId = languages.find(l => l.folderName == locFolderName).id;
     let outputPath = path.join(cpptoolsRepoPath, "Extension/bin", `edge_strings.${languageId}.json`);
     let sourceContent = fs.readFileSync(lclPath, 'utf-8');
