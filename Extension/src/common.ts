febaadb20398d9edb4588d51a2ff7d5f9d34eaff
dx-coding <58005544+dx-coding@users.noreply.github.com>
2019-11-25 12:36:46 -0500

    Update common.ts

--- Extension/src/common.ts
+++ Extension/src/common.ts
@@ -633,9 +633,13 @@ export function spawnChildProcess(process: string, args: string[], workingDirect
 }
 
 export function isExecutable(file: string): Promise<boolean> {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         fs.access(file, fs.constants.X_OK, (err) => {
-            resolve(err);
+            if(err) {
+                resolve(true);
+            else {
+                resolve(false);
+            }
         });
     });
 }
