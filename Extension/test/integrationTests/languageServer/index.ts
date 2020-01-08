78b09612e93ff7c87d21f084802e1c707c62e431
Michelle Matias <mimatias@microsoft.com>
2020-01-07 17:26:55 -0800

    increase default timeout

--- Extension/test/integrationTests/languageServer/index.ts
+++ Extension/test/integrationTests/languageServer/index.ts
@@ -28,7 +28,7 @@ testRunner.configure({
     ui: 'tdd', 		// the TDD UI is being used in *.test.ts (suite, test, etc.)
     useColors: true, // colored output from test results
     fullStackTrace: true,
-    timeout: 60000
+    timeout: 90000
 });
 
 const logFolder: string = path.join(__dirname, ".logs");
