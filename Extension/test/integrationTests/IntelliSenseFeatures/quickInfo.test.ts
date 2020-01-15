7b3e9827847ffb5512956789be21fd81bb044925
Michelle Matias <mimatias@microsoft.com>
2020-01-14 17:37:23 -0800

    set result per platform

--- Extension/test/integrationTests/IntelliSenseFeatures/quickInfo.test.ts
+++ Extension/test/integrationTests/IntelliSenseFeatures/quickInfo.test.ts
@@ -5,6 +5,7 @@
 
 import * as vscode from 'vscode';
 import * as assert from 'assert';
+import os from 'os';
 import * as api from 'vscode-cpptools';
 import * as apit from 'vscode-cpptools/out/testApi';
 import { activateCppExtension } from '../testHelpers';
@@ -16,41 +17,51 @@ suite("[Quick info test]", function(): void {
     let disposables: vscode.Disposable[] = [];
     let filePath: string = vscode.workspace.workspaceFolders[0].uri.fsPath + "/quickInfo.cpp";
     let fileUri: vscode.Uri = vscode.Uri.file(filePath);
+    let platform: string = "";
 
     suiteSetup(async function(): Promise<void> {
         await activateCppExtension();
 
         // TODO: create common function to start language server that accepts source file as input
         cpptools = await apit.getCppToolsTestApi(api.Version.latest);
-
+        platform = os.platform();
         let testHook: apit.CppToolsTestHook = cpptools.getTestHook();
         disposables.push(testHook);
 
-        let testResult: any = new Promise<void>((resolve, reject) => {
+        let getIntelliSenseStatus: any = new Promise<void>((resolve, reject) => {
             disposables.push(testHook.IntelliSenseStatusChanged(result => {
                 result = result as apit.IntelliSenseStatus;
                 if (result.filename === "quickInfo.cpp" && result.status === apit.Status.IntelliSenseReady) {
                     resolve();
                 }
             }));
-            setTimeout(() => { reject(new Error("timeout")); }, defaultTimeout);
+            setTimeout(() => { reject(new Error("Timeout: IntelliSenseStatusChanged event")); }, defaultTimeout);
         });
 
         // Start language server
+        console.log("Open file: " + fileUri.toString());
         await vscode.commands.executeCommand("vscode.open", fileUri);
-        await testResult;
+        await getIntelliSenseStatus;
     });
 
     suiteTeardown(function(): void {
         disposables.forEach(d => d.dispose());
     });
 
-    test("Hover over function call", async () => {
+    test("[Hover over function call]", async () => {
         let result: vscode.Hover[] = <vscode.Hover[]>(await vscode.commands.executeCommand('vscode.executeHoverProvider', fileUri, new vscode.Position(12, 12)));
-        let expected1: string =
-        `\`\`\`cpp
-void myfunction(int var1, std::string var2, std::string var3)
-\`\`\``;
+        let expected1: string = "";
+        switch (platform) {
+            case "win32":
+                expected1 = `\`\`\`cpp\nvoid myfunction(int var1, std::string var2, std::string var3)\n\`\`\``;
+                break;
+            case "linux":
+                expected1 = `\`\`\`cpp\nvoid myfunction(int var1, std::__cxx11::string var2, std::__cxx11::string var3)\n\`\`\``;
+                break;
+            case "darwin":
+                expected1 = `\`\`\`cpp\nvoid myfunction(int var1, std::__1::string var2, std::__1::string var3)\n\`\`\``;
+                break;
+        }
         let actual1: string = (<vscode.MarkdownString>result[0].contents[0]).value;
         assert.equal(actual1, expected1);
         let expected2: string = `comment for myfunction`;
@@ -58,31 +69,43 @@ void myfunction(int var1, std::string var2, std::string var3)
         assert.equal(actual2, expected2);
     });
 
-    test("Hover over function param string variable", async () => {
+    test("[Hover over function param string variable]", async () => {
         let result: vscode.Hover[] = <vscode.Hover[]>(await vscode.commands.executeCommand('vscode.executeHoverProvider', fileUri, new vscode.Position(12, 30)));
-        let expected: string =
-        `\`\`\`cpp
-std::string stringVar
-\`\`\``;
+        let expected: string = "";
+        switch (platform) {
+            case "win32":
+                expected = `\`\`\`cpp\nstd::string stringVar\n\`\`\``;
+                break;
+            case "linux":
+                expected = `\`\`\`cpp\nstd::__cxx11::string stringVar\n\`\`\``;
+                break;
+            case "darwin":
+                expected = `\`\`\`cpp\nstd::__1::string stringVar\n\`\`\``;
+                break;
+        }
         let actual: string = (<vscode.MarkdownString>result[0].contents[0]).value;
-
         assert.equal(actual, expected);
     });
 
-    test("Hover over function param string literal", async () => {
+    test("[Hover over function param string literal]", async () => {
         let result: vscode.Hover[] = <vscode.Hover[]>(await vscode.commands.executeCommand('vscode.executeHoverProvider', fileUri, new vscode.Position(12, 44)));
-        let expected: string =
-        `\`\`\`cpp
-std::string::basic_string(const char *_Ptr)
-\`\`\`
-
-+17 overloads
-`;
+        let expected: string = "";
+        switch (platform) {
+            case "win32":
+                expected = `\`\`\`cpp\nstd::string::basic_string(const char *_Ptr)\n\`\`\`\n\n+17 overloads\n\`\`\``;
+                break;
+            case "linux":
+                expected = `\`\`\`cpp\nstd::__cxx11::string::basic_string(const char *__s, const std::allocator<...> &__a = std::allocator<...>())\n\`\`\`\n\n+16 overloads\n`;
+                break;
+            case "darwin":
+                expected = `\`\`\`cpp\nstd::__1::string::basic_string<std::nullptr_t>(const char *__s)\n\`\`\`\n\n+21 overloads\n`;
+                break;
+        }
         let actual: string = (<vscode.MarkdownString>result[0].contents[0]).value;
         assert.equal(actual, expected);
     });
 
-    test("Hover over function param with squiggles", async () => {
+    test("[Hover over function param with squiggles]", async () => {
         let result: vscode.Hover[] = <vscode.Hover[]>(await vscode.commands.executeCommand('vscode.executeHoverProvider', fileUri, new vscode.Position(13, 18)));
         let expected: string = `\`\`\`cpp\nint intVar\n\`\`\``;
         let actual: string = (<vscode.MarkdownString>result[0].contents[0]).value;
