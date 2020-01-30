2aeb48d722fe7806b743d11e069df35e37f7eae1
aleksey-sergey <sergey.aleksey90@gmail.com>
2020-01-30 03:46:37 +0100

    add support for a new command: cpptools.setActiveConfigName (#4893)
    * add support for a new command: cpptools.setActiveConfigName
    
    Command allows other extensions to control activeConfigName
    
    * post review fix: localized config-name-not-found message

--- Extension/src/LanguageServer/extension.ts
+++ Extension/src/LanguageServer/extension.ts
@@ -894,6 +894,7 @@ export function registerCommands(): void {
     disposables.push(vscode.commands.registerCommand('C_Cpp.VcpkgClipboardInstallSuggested', onVcpkgClipboardInstallSuggested));
     disposables.push(vscode.commands.registerCommand('C_Cpp.VcpkgOnlineHelpSuggested', onVcpkgOnlineHelpSuggested));
     disposables.push(vscode.commands.registerCommand('cpptools.activeConfigName', onGetActiveConfigName));
+    disposables.push(vscode.commands.registerCommand('cpptools.setActiveConfigName', onSetActiveConfigName));
     getTemporaryCommandRegistrarInstance().executeDelayedCommands();
 }
 
@@ -1145,6 +1146,10 @@ async function onVcpkgClipboardInstallSuggested(ports?: string[]): Promise<void>
     telemetry.logLanguageServerEvent('vcpkgAction', { 'source': source, 'action': 'vcpkgClipboardInstallSuggested', 'ports': ports.toString() });
 
     await vscode.env.clipboard.writeText(installCommand);
+}
+
+function onSetActiveConfigName(configurationName: string) : Thenable<void> {
+    return clients.ActiveClient.setCurrentConfigName(configurationName);
 }
 
 function onGetActiveConfigName(): Thenable<string> {
