b4e0c183fe237c4458250d7e182f905209f68b66
Pierson Lee <piel@microsoft.com>
2020-03-05 14:13:21 -0800

    Adding the arm/arm64 packages for mono runtime
    Adding the arm/arm64 packages for the mono runtime. This is to enable
    remote-ssh extension debugging.

--- Extension/src/platform.ts
+++ Extension/src/platform.ts
@@ -71,6 +71,12 @@ export class PlatformInformation {
         return util.execChildProcess('uname -m', util.packageJson.extensionFolderPath)
             .then((architecture) => {
                 if (architecture) {
+                    if (architecture.startsWith('arm64') || architecture.startsWith('aarch64')) {
+                        return 'arm64';
+                    } else if (architecture.startsWith('armv')) {
+                        return 'arm';
+                    } 
+
                     return architecture.trim();
                 }
                 return null;
