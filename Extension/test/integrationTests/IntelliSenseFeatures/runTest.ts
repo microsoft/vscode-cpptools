a3b7c7654bf68fd453f643aec7c208626628f879
Colen M. Garoutte-Carson <coleng@microsoft.com>
2020-03-17 17:50:14 -0700

    Fix spacing in new test sources

--- Extension/test/integrationTests/IntelliSenseFeatures/runTest.ts
+++ Extension/test/integrationTests/IntelliSenseFeatures/runTest.ts
@@ -3,23 +3,23 @@ import * as path from 'path';
 import { runTests } from 'vscode-test';
 
 async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../../../');
+    try {
+        // The folder containing the Extension Manifest package.json
+        // Passed to `--extensionDevelopmentPath`
+        const extensionDevelopmentPath = path.resolve(__dirname, '../../../../');
 
-		// The path to the extension test script
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './index');
+        // The path to the extension test script
+        // Passed to --extensionTestsPath
+        const extensionTestsPath = path.resolve(__dirname, './index');
 
-		const launchArgs = [ "--disable-extensions" ];
+        const launchArgs = [ "--disable-extensions" ];
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ launchArgs, extensionDevelopmentPath, extensionTestsPath });
-	} catch (err) {
-		console.error('Failed to run tests');
-		process.exit(1);
-	}
+        // Download VS Code, unzip it and run the integration test
+        await runTests({ launchArgs, extensionDevelopmentPath, extensionTestsPath });
+    } catch (err) {
+        console.error('Failed to run tests');
+        process.exit(1);
+    }
 }
 
 main();
--- Extension/test/unitTests/index.ts
+++ Extension/test/unitTests/index.ts
@@ -3,36 +3,36 @@ import * as Mocha from 'mocha';
 import * as glob from 'glob';
 
 export function run(): Promise<void> {
-  // Create the mocha test
-  const mocha = new Mocha({
-    ui: 'tdd'
-  });
-  mocha.useColors(true);
-
-  const testsRoot = __dirname;
-
-  return new Promise((c, e) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return e(err);
-      }
-
-      // Add files to the test suite
-      files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-      try {
-        // Run the mocha test
-        mocha.timeout(100000);
-        mocha.run(failures => {
-          if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
-          } else {
-            c();
-          }
-        });
-      } catch (err) {
-        e(err);
-      }
+    // Create the mocha test
+    const mocha = new Mocha({
+        ui: 'tdd'
     });
-  });
+    mocha.useColors(true);
+
+    const testsRoot = __dirname;
+
+    return new Promise((c, e) => {
+        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+            if (err) {
+                return e(err);
+            }
+
+            // Add files to the test suite
+            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+            try {
+                // Run the mocha test
+                mocha.timeout(100000);
+                mocha.run(failures => {
+                    if (failures > 0) {
+                        e(new Error(`${failures} tests failed.`));
+                    } else {
+                        c();
+                    }
+                });
+            } catch (err) {
+                e(err);
+            }
+        });
+    });
 }
