873775aafe4ee00beab2082c1d9b9d394eb850fa
Elaheh Rashedi <fp0634@wayne.edu>
2020-04-01 12:00:42 -0700

    define path delimiter based on windows/mac config (#5229)
    * define path delimiter based on windows/mac config
    https://github.com/microsoft/vscode-cpptools/issues/3940

--- Extension/src/LanguageServer/configurations.ts
+++ Extension/src/LanguageServer/configurations.ts
@@ -523,8 +523,9 @@ export class CppProperties {
         let result: string[] = [];
         if (paths) {
             paths = this.resolveDefaults(paths, defaultValue);
+            let delimiter: string = (process.platform === 'win32') ? ";" : ":";
             paths.forEach(entry => {
-                let entries: string[] = util.resolveVariables(entry, env).split(";").filter(e => e);
+                let entries: string[] = util.resolveVariables(entry, env).split(delimiter).filter(e => e);
                 result = result.concat(entries);
             });
         }
