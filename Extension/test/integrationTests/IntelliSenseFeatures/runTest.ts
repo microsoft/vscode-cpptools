2d518a1287f108d54c3f78a4b27a986e9a85be2e
Michelle Matias <mimatias@microsoft.com>
2020-04-09 11:10:50 -0700

    log test variables

--- Extension/test/integrationTests/IntelliSenseFeatures/runTest.ts
+++ Extension/test/integrationTests/IntelliSenseFeatures/runTest.ts
@@ -4,16 +4,31 @@ import { runTests } from 'vscode-test';
 
 async function main() {
     try {
+        console.log("__dirname: " + __dirname);
+        
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
         const extensionDevelopmentPath = path.resolve(__dirname, '../../../../');
+        console.log("extensionDevelopmentPath: " + extensionDevelopmentPath);
 
         // The path to the extension test script
         // Passed to --extensionTestsPath
         const extensionTestsPath = path.resolve(__dirname, './index');
 
+        let extensionPath: string | undefined = process.env.EXTENSIONS_PATH;
+        if (!extensionPath) {
+            console.error("Unable to read process.env.EXTENSIONS_PATH");
+        } else {
+            console.log("extensionPath: " + extensionPath);
+        }
+
         // Note, when running tests locally, replace testWorkspace with local path to "~/Vcls-vscode-test/SingleRootProject"
-        const testWorkspace = path.relative(extensionDevelopmentPath, '../../../../../../Vcls-vscode-test/SingleRootProject');
+        let testWorkspace: string | undefined = process.env.TESTS_WORKSPACE;
+        if (!testWorkspace) {
+            console.error("Unable to read process.env.TESTS_WORKSPACE");
+        } else {
+            console.log("testWorkspace: " + testWorkspace);
+        }
 
         const launchArgs = [ testWorkspace, "--disable-extensions" ];
 
