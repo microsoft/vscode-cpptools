863b0c29550c368fa95a78dca3344f702c1b58d4
Michelle Matias <mimatias@microsoft.com>
2020-04-10 11:57:28 -0700

    clean up runTest scripts

--- Extension/test/integrationTests/IntelliSenseFeatures/runTest.ts
+++ Extension/test/integrationTests/IntelliSenseFeatures/runTest.ts
@@ -5,7 +5,7 @@ import { runTests } from 'vscode-test';
 async function main() {
     try {
         console.log("__dirname: " + __dirname);
-        
+
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
         const extensionDevelopmentPath = path.resolve(__dirname, '../../../../');
@@ -15,14 +15,8 @@ async function main() {
         // Passed to --extensionTestsPath
         const extensionTestsPath = path.resolve(__dirname, './index');
 
-        let extensionPath: string | undefined = process.env.EXTENSIONS_PATH;
-        if (!extensionPath) {
-            console.error("Unable to read process.env.EXTENSIONS_PATH");
-        } else {
-            console.log("extensionPath: " + extensionPath);
-        }
-
         // Note, when running tests locally, replace testWorkspace with local path to "~/Vcls-vscode-test/SingleRootProject"
+        // in the Launch.json file.
         let testWorkspace: string | undefined = process.env.TESTS_WORKSPACE;
         if (!testWorkspace) {
             console.error("Unable to read process.env.TESTS_WORKSPACE");
@@ -30,7 +24,7 @@ async function main() {
             console.log("testWorkspace: " + testWorkspace);
         }
 
-        const launchArgs = [ testWorkspace, "--disable-extensions" ];
+        const launchArgs = [ "--disable-extensions", testWorkspace ];
 
         // Download VS Code, unzip it and run the integration test
         await runTests({ launchArgs, extensionDevelopmentPath, extensionTestsPath });
--- Extension/test/integrationTests/languageServer/runTest.ts
+++ Extension/test/integrationTests/languageServer/runTest.ts
@@ -7,21 +7,14 @@ async function main() {
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
         const extensionDevelopmentPath = path.resolve(__dirname, '../../../../');
-        console.log("extensionDevelopmentPath: " + extensionDevelopmentPath);
-        console.log("__dirname: " + __dirname);
+
         // The path to the extension test script
         // Passed to --extensionTestsPath
         const extensionTestsPath = path.resolve(__dirname, './index');
 
-        //const testWorkspace = path.resolve(extensionDevelopmentPath, 'test/integrationTests/testAssets/SimpleCppProject');
-        let testWorkspace2: string | undefined = process.env.TESTS_WORKSPACE;
-        if (!testWorkspace2) {
-            console.error("Unable to read process.env.TESTS_WORKSPACE");
-        } else {
-            console.log("testWorkspace: " + testWorkspace2);
-        }
+        const testWorkspace = path.resolve(extensionDevelopmentPath, 'test/integrationTests/testAssets/SimpleCppProject');
 
-        const launchArgs = [ "--disable-extensions", testWorkspace2 ];
+        const launchArgs = [ "--disable-extensions", testWorkspace ];
 
         // Download VS Code, unzip it and run the integration test
         await runTests({ launchArgs, extensionDevelopmentPath, extensionTestsPath });
