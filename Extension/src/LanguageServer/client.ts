6a1cfbe1b04b6a64eb2ea1bc73a87957ccae250d
Colen Garoutte-Carson <coleng@microsoft.com>
2020-05-06 16:35:09 -0700

    Add telemetry event for main process crashes

--- Extension/src/LanguageServer/client.ts
+++ Extension/src/LanguageServer/client.ts
@@ -1240,6 +1240,7 @@ export class DefaultClient implements Client {
                 closed: () => {
                     languageClientCrashTimes.push(Date.now());
                     languageClientCrashedNeedsRestart = true;
+                    telemetry.logLanguageServerEvent("languageClientCrash", { "count": languageClientCrashTimes.length.toString() }, undefined);
                     if (languageClientCrashTimes.length < 5) {
                         allClients.forEach(client => { allClients.replace(client, true); });
                     } else {
