465f00760898fb3ffba31fdd9de89995d5934fab
Elaheh Rashedi <elrashed@microsoft.com>
2020-09-01 12:45:02 -0700

    fix linter errors

--- Extension/src/common.ts
+++ Extension/src/common.ts
@@ -640,11 +640,11 @@ export function readFileText(filePath: string, encoding: string = "utf8"): Promi
 
 /** Writes content to a text file */
 export function writeFileText(filePath: string, content: string, encoding: string = "utf8"): Promise<void> {
-    const folders = filePath.split(path.sep).slice(0, -1);
+    const folders: string[] = filePath.split(path.sep).slice(0, -1);
     if (folders.length) {
         // create folder path if it doesn't exist
         folders.reduce((last, folder) => {
-            const folderPath = last ? last + path.sep + folder : folder;
+            const folderPath: string = last ? last + path.sep + folder : folder;
             if (!fs.existsSync(folderPath)) {
                 fs.mkdirSync(folderPath);
             }
