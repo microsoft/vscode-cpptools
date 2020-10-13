1d7e3f7c8962b7edc3b44b5b67434a506a172d1f
Andrew Wang <waan@microsoft.com>
2020-10-13 13:10:37 -0700

    Update configurationProvider.ts

--- Extension/src/Debugger/configurationProvider.ts
+++ Extension/src/Debugger/configurationProvider.ts
@@ -274,6 +274,8 @@ class CppConfigurationProvider implements vscode.DebugConfigurationProvider {
             const outputChannel: logger.Logger = logger.getOutputChannelLogger();
             outputChannel.appendLine(localize("debugger.launchConfig", "Launch configuration:"));
             outputChannel.appendLine(JSON.stringify(config, undefined, 2));
+	    // TODO: Enable when https://github.com/microsoft/vscode/issues/108619 is resolved.
+	    // logger.showOutputChannel();
         }
 
         return config;
