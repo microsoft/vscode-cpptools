7349aa5449c0d16e33a027d320ad3a575efca071
Sean McManus <seanmcm@microsoft.com>
2020-12-23 10:47:34 -0800

    Fix other cppbuild issues.

--- Extension/src/LanguageServer/cppBuildTaskProvider.ts
+++ Extension/src/LanguageServer/cppBuildTaskProvider.ts
@@ -374,38 +374,36 @@ class CustomBuildTaskTerminal implements Pseudoterminal {
         try {
             const result: number = await new Promise<number>((resolve, reject) => {
                 cp.exec(activeCommand, this.options, (_error, stdout, _stderr) => {
-                    const dot: string = (stdout || _stderr) ? ":" : ".";
+                    const dot: string = ".";
+                    const is_cl: boolean = (_stderr || _stderr === '') && stdout ? true : false;
+                    if (is_cl) {
+                        // cl.exe, header info may not appear if /nologo is used.
+                        if (_stderr) {
+                            splitWriteEmitter(_stderr); // compiler header info and command line D warnings
+                        }
+                        splitWriteEmitter(stdout); // linker header info and potentially compiler C warnings
+                    }
                     if (_error) {
-                        telemetry.logLanguageServerEvent("cppBuildTaskError");
-                        this.writeEmitter.fire(localize("build_finished_with_error", "Build finished with errors(s)") + dot + this.endOfLine);
                         if (stdout) {
-                            splitWriteEmitter(stdout); // cl.exe
+                            // cl.exe
                         } else if (_stderr) {
                             splitWriteEmitter(_stderr); // gcc/clang
                         } else {
                             splitWriteEmitter(_error.message); // e.g. command executable not found
                         }
+                        telemetry.logLanguageServerEvent("cppBuildTaskError");
+                        this.writeEmitter.fire(localize("build_finished_with_error", "Build finished with error(s)") + dot + this.endOfLine);
                         resolve(-1);
-                        return;
                     } else if (_stderr && !stdout) { // gcc/clang
+                        splitWriteEmitter(_stderr);
                         telemetry.logLanguageServerEvent("cppBuildTaskWarnings");
                         this.writeEmitter.fire(localize("build_finished_with_warnings", "Build finished with warning(s)") + dot + this.endOfLine);
-                        splitWriteEmitter(_stderr);
                         resolve(0);
                     } else if (stdout && stdout.includes("warning C")) { // cl.exe, compiler warnings
                         telemetry.logLanguageServerEvent("cppBuildTaskWarnings");
                         this.writeEmitter.fire(localize("build_finished_with_warnings", "Build finished with warning(s)") + dot + this.endOfLine);
-                        splitWriteEmitter(stdout);
-                        resolve(0);
-                    } else if (_stderr && _stderr.includes("warning D")) { // cl.exe, command line warnings
-                        telemetry.logLanguageServerEvent("cppBuildTaskWarnings");
-                        this.writeEmitter.fire(localize("build_finished_with_warnings", "Build finished with warning(s)") + dot + this.endOfLine);
-                        splitWriteEmitter(_stderr);
                         resolve(0);
                     } else {
-                        if (stdout) {
-                            splitWriteEmitter(stdout); // cl.exe
-                        }
                         this.writeEmitter.fire(localize("build finished successfully", "Build finished successfully.") + this.endOfLine);
                         resolve(0);
                     }
