bf6dace055f049d4753c9c393344485c1324b7d9
Sean McManus <seanmcm@microsoft.com>
2021-02-11 02:21:15 -0800

    Fix bug with cwd in build tasks.

--- Extension/src/LanguageServer/cppBuildTaskProvider.ts
+++ Extension/src/LanguageServer/cppBuildTaskProvider.ts
@@ -169,7 +169,7 @@ export class CppBuildTaskProvider implements TaskProvider {
             if (compilerArgs && compilerArgs.length > 0) {
                 args = args.concat(compilerArgs);
             }
-            const cwd: string = isWindows && !isCl && !process.env.PATH?.includes(compilerPath) ? path.dirname(compilerPath) : "${workspaceFolder}";
+            const cwd: string = isWindows && !isCl && !process.env.PATH?.includes(path.dirname(compilerPath)) ? path.dirname(compilerPath) : "${workspaceFolder}";
             const options: cp.ExecOptions | undefined = { cwd: cwd };
             definition = {
                 type: CppBuildTaskProvider.CppBuildScriptType,
