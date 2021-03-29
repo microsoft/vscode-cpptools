104286e830618e6796276c8f5dfbe16c8f0ea3a4
Sean McManus <seanmcm@microsoft.com>
2021-03-29 07:33:18 -0700

    Fix watch mode.

--- Extension/webpack.config.js
+++ Extension/webpack.config.js
@@ -28,7 +28,8 @@ const config = {
         vscode: "commonjs vscode" // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     },
     resolve: { // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-        extensions: ['.ts', '.js']
+        extensions: ['.ts', '.js'],
+        mainFields: ['main', 'module']
     },
     module: {
         rules: [{
