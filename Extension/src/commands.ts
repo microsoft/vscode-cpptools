c4aa3196cff298d972515d863a46291000a9092b
Sean McManus <seanmcm@microsoft.com>
2021-03-29 09:58:35 -0700

    Undo change.

--- Extension/src/commands.ts
+++ Extension/src/commands.ts
@@ -20,7 +20,6 @@ class TemporaryCommandRegistrar {
     private isActivationReady: boolean = false;
 
     private commandsToRegister: string[] = [
-        "C_Cpp.BuildAndDebugActiveFile",
         "C_Cpp.ConfigurationEditJSON",
         "C_Cpp.ConfigurationEditUI",
         "C_Cpp.ConfigurationSelect",
