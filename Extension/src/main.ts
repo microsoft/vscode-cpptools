2ed6660b5a0a37b4a140a4dbd6c26a729b2e5f08
Colen Garoutte-Carson <coleng@microsoft.com>
2021-04-06 14:11:55 -0700

    Add check and message for x64 VSIX on M1 Mac

--- Extension/src/main.ts
+++ Extension/src/main.ts
@@ -76,37 +76,57 @@ export async function activate(context: vscode.ExtensionContext): Promise<CppToo
     await processRuntimeDependencies();
 
     const installedPlatform: string | undefined = util.getInstalledBinaryPlatform();
+    const promptForMacArchictureMismatch: PersistentState<boolean> = new PersistentState<boolean>("CPP.promptForMacArchictureMismatch", true);
 
     // Check the main binaries files to declare if the extension has been installed successfully.
-    if (installedPlatform && process.platform !== installedPlatform) {
+    if (installedPlatform && process.platform !== installedPlatform || arch !== process.arch) {
         // Check if the correct offline/insiders vsix is installed on the correct platform.
         const platformInfo: PlatformInformation = await PlatformInformation.GetPlatformInformation();
         const vsixName: string = vsixNameForPlatform(platformInfo);
-        errMsg = localize("native.binaries.not.supported", "This {0} version of the extension is incompatible with your OS. Please download and install the \"{1}\" version of the extension.", GetOSName(installedPlatform), vsixName);
         const downloadLink: string = localize("download.button", "Go to Download Page");
-        vscode.window.showErrorMessage(errMsg, downloadLink).then(async (selection) => {
-            if (selection === downloadLink) {
-                vscode.env.openExternal(vscode.Uri.parse(releaseDownloadUrl));
+        if (installedPlatform === 'darwin' && process.arch === "x64" && arch === "arm64") {
+            if (promptForMacArchictureMismatch.Value) {
+                // Display a message specifically referring the user to the ARM64 Mac build on ARM64 Mac.
+                errMsg = localize("native.binaries.mismatch.osx", "This {0} version of the extension has been installed.  Since you are on an Apple Silicon Mac, we recommend installing the \"{1}\" version of the extension.");
+                promptForMacArchictureMismatch.Value = false;
+                vscode.window.showErrorMessage(errMsg, downloadLink).then(async (selection) => {
+                    if (selection === downloadLink) {
+                        vscode.env.openExternal(vscode.Uri.parse(releaseDownloadUrl));
+                    }
+                });
             }
-        });
-    } else if (!(await util.checkInstallBinariesExist())) {
-        errMsg = localize("extension.installation.failed", "The C/C++ extension failed to install successfully. You will need to repair or reinstall the extension for C/C++ language features to function properly.");
-        const reload: string = localize("remove.extension", "Attempt to Repair");
-        vscode.window.showErrorMessage(errMsg, reload).then(async (value?: string) => {
-            if (value === reload) {
-                await util.removeInstallLockFile();
-                vscode.commands.executeCommand("workbench.action.reloadWindow");
-            }
-        });
-    } else if (!(await util.checkInstallJsonsExist())) {
-        // Check the Json files to declare if the extension has been installed successfully.
-        errMsg = localize("jason.files.missing", "The C/C++ extension failed to install successfully. You will need to reinstall the extension for C/C++ language features to function properly.");
-        const downloadLink: string = localize("download.button", "Go to Download Page");
-        vscode.window.showErrorMessage(errMsg, downloadLink).then(async (selection) => {
-            if (selection === downloadLink) {
-                vscode.env.openExternal(vscode.Uri.parse(releaseDownloadUrl));
-            }
-        });
+        } else {
+            // Reset the persistent boolean tracking whether to warn the user of architecture mismatch on OSX.
+            promptForMacArchictureMismatch.Value = true;
+            errMsg = localize("native.binaries.not.supported", "This {0} version of the extension is incompatible with your OS. Please download and install the \"{1}\" version of the extension.", GetOSName(installedPlatform), vsixName);
+            vscode.window.showErrorMessage(errMsg, downloadLink).then(async (selection) => {
+                if (selection === downloadLink) {
+                    vscode.env.openExternal(vscode.Uri.parse(releaseDownloadUrl));
+                }
+            });
+        }
+    } else {
+        // Reset the persistent boolean tracking whether to warn the user of architecture mismatch on OSX.
+        promptForMacArchictureMismatch.Value = true;
+        if (!(await util.checkInstallBinariesExist())) {
+            errMsg = localize("extension.installation.failed", "The C/C++ extension failed to install successfully. You will need to repair or reinstall the extension for C/C++ language features to function properly.");
+            const reload: string = localize("remove.extension", "Attempt to Repair");
+            vscode.window.showErrorMessage(errMsg, reload).then(async (value?: string) => {
+                if (value === reload) {
+                    await util.removeInstallLockFile();
+                    vscode.commands.executeCommand("workbench.action.reloadWindow");
+                }
+            });
+        } else if (!(await util.checkInstallJsonsExist())) {
+            // Check the Json files to declare if the extension has been installed successfully.
+            errMsg = localize("jason.files.missing", "The C/C++ extension failed to install successfully. You will need to reinstall the extension for C/C++ language features to function properly.");
+            const downloadLink: string = localize("download.button", "Go to Download Page");
+            vscode.window.showErrorMessage(errMsg, downloadLink).then(async (selection) => {
+                if (selection === downloadLink) {
+                    vscode.env.openExternal(vscode.Uri.parse(releaseDownloadUrl));
+                }
+            });
+        }
     }
 
     return cppTools;
