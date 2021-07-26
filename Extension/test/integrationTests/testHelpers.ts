e8ba77c3c4f5d72fa1487d0d7f223b9c773502d3
Sean McManus <seanmcm@microsoft.com>
2021-07-26 14:06:50 -0700

    Force activation.

--- Extension/test/integrationTests/testHelpers.ts
+++ Extension/test/integrationTests/testHelpers.ts
@@ -8,9 +8,9 @@ export const defaultTimeout: number = 100000;
 
 export async function activateCppExtension(): Promise<void> {
     const extension: vscode.Extension<any> = vscode.extensions.getExtension("ms-vscode.cpptools");
-    if (!extension.isActive) {
-        await extension.activate();
-    }
+    // if (!extension.isActive) {
+    await extension.activate();
+    // }
 }
 export function delay(ms: number): Promise<void> {
     return new Promise<void>(resolve => setTimeout(resolve, ms));
