44edc8fee7a02835b678aa66e4c3de0f51bed27d
elrashed <elahehrashedi@gmail.com>
2021-08-31 10:37:45 -0700

    skip relative paths

--- Extension/src/LanguageServer/configurations.ts
+++ Extension/src/LanguageServer/configurations.ts
@@ -1397,7 +1397,7 @@ export class CppProperties {
         errors.browsePath = this.validatePath(config.browse ? config.browse.path : undefined);
 
         // Validate files
-        errors.forcedInclude = this.validatePath(config.forcedInclude, false);
+        errors.forcedInclude = this.validatePath(config.forcedInclude, false, true);
         errors.compileCommands = this.validatePath(config.compileCommands, false);
         errors.databaseFilename = this.validatePath((config.browse ? config.browse.databaseFilename : undefined), false);
 
@@ -1412,7 +1412,7 @@ export class CppProperties {
         return errors;
     }
 
-    private validatePath(input: string | string[] | undefined, isDirectory: boolean = true): string | undefined {
+    private validatePath(input: string | string[] | undefined, isDirectory: boolean = true, skipRelativePaths: boolean = false): string | undefined {
         if (!input) {
             return undefined;
         }
@@ -1440,7 +1440,9 @@ export class CppProperties {
 
             // Check if resolved path exists
             if (!fs.existsSync(resolvedPath)) {
-                if (!this.rootUri) {
+                if (skipRelativePaths) {
+                    continue;
+                } else if (!this.rootUri) {
                     pathExists = false;
                 } else {
                     // Check for relative path if resolved path does not exists
@@ -1622,10 +1624,19 @@ export class CppProperties {
             let paths: string[] = [];
             let compilerPath: string | undefined;
             for (const pathArray of [ (currentConfiguration.browse ? currentConfiguration.browse.path : undefined),
-                currentConfiguration.includePath, currentConfiguration.macFrameworkPath, currentConfiguration.forcedInclude ]) {
+                currentConfiguration.includePath, currentConfiguration.macFrameworkPath ]) {
                 if (pathArray) {
                     for (const curPath of pathArray) {
                         paths.push(`${curPath}`);
+                    }
+                }
+            }
+            // Skip the relative forcedInclude files.
+            if (currentConfiguration.forcedInclude) {
+                for (const file of currentConfiguration.forcedInclude) {
+                    const resolvedFilePath: string = this.resolvePath(file, isWindows);
+                    if (util.checkFileExistsSync(resolvedFilePath) || util.checkDirectoryExistsSync(resolvedFilePath)) {
+                        paths.push(`${file}`);
                     }
                 }
             }
