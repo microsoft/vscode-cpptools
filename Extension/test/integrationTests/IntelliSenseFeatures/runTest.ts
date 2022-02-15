475c2f29a162a4fbc7e8eb7d86954e9123d8ad0a
Sean McManus <seanmcm@microsoft.com>
2022-02-15 15:42:31 -0800

    Attempt to get more error info.

--- Extension/test/integrationTests/IntelliSenseFeatures/runTest.ts
+++ Extension/test/integrationTests/IntelliSenseFeatures/runTest.ts
@@ -27,6 +27,7 @@ async function main() {
         await runTests({ launchArgs, extensionDevelopmentPath, extensionTestsPath });
     } catch (err) {
         console.error('Failed to run tests');
+        console.error(err);
         process.exit(1);
     }
 }
