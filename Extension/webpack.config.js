7e5cfae7d81092614e2ba3ead8fbc41930a65f2e
Colen Garoutte-Carson <coleng@microsoft.com>
2022-03-03 19:41:17 -0800

    Remove wordexp from webpack

--- Extension/webpack.config.js
+++ Extension/webpack.config.js
@@ -25,8 +25,7 @@ const config = {
     },
     devtool: 'source-map',
     externals: {
-        vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-        wordexp: "wordexp"
+        vscode: "commonjs vscode" // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     },
     resolve: { // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
         extensions: ['.ts', '.js'],
