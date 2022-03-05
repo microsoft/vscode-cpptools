2ab49d4f8fc556acefe192ea97dbd61410db5743
Colen Garoutte-Carson <coleng@microsoft.com>
2022-03-04 23:04:15 -0800

    Add cpptools_wordexp to makeBinariesExecutable

--- Extension/src/main.ts
+++ Extension/src/main.ts
@@ -100,6 +100,7 @@ async function makeBinariesExecutable(): Promise<void> {
         const commonBinaries: string[] = [
             "./bin/cpptools",
             "./bin/cpptools-srv",
+            "./bin/cpptools_wordexp",
             "./LLVM/bin/clang-format",
             "./LLVM/bin/clang-tidy",
             "./debugAdapters/bin/OpenDebugAD7"
