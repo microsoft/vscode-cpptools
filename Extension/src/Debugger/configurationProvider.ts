5894e687952c813b5cae5b7cae948883b02b0fd5
ChaseKnowlden <haroldknowlden@gmail.com>
2022-07-19 16:49:09 -0400

    Fix CI

--- Extension/src/Debugger/configurationProvider.ts
+++ Extension/src/Debugger/configurationProvider.ts
@@ -857,7 +857,7 @@ export class DebugConfigurationProvider implements vscode.DebugConfigurationProv
 
     public async buildAndDebug(textEditor: vscode.TextEditor, debugModeOn: boolean = true): Promise<void> {
         let folder: vscode.WorkspaceFolder | undefined = vscode.workspace.getWorkspaceFolder(textEditor.document.uri);
-        let err: boolean = false;
+        const err: boolean = false;
 
         const selectedConfig: CppDebugConfiguration | undefined = await this.selectConfiguration(textEditor);
         if (!selectedConfig) {
@@ -875,7 +875,11 @@ export class DebugConfigurationProvider implements vscode.DebugConfigurationProv
         }
         selectedConfig.debugType = debugModeOn ? DebugType.debug : DebugType.run;
         // startDebugging will trigger a call to resolveDebugConfiguration.
-        err = await vscode.debug.startDebugging(folder, selectedConfig, { noDebug: !debugModeOn });
+        if (err) {
+            throw new Error(localize("debugger.not.installed", "The debugger you're using is not installed. Please install the debugger first."));
+        } else {
+            await vscode.debug.startDebugging(folder, selectedConfig, { noDebug: !debugModeOn });
+        }
     }
 
     private async selectConfiguration(textEditor: vscode.TextEditor, pickDefault: boolean = true, onlyWorkspaceFolder: boolean = false): Promise<CppDebugConfiguration | undefined> {
