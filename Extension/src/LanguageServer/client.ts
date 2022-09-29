a28d57d6cf5b0a0b067398c31022e247047d3e11
Colen Garoutte-Carson <coleng@microsoft.com>
2022-09-29 15:15:22 -0700

    Ensure workspaceFolderUri contains a uri and not a file path

--- Extension/src/LanguageServer/client.ts
+++ Extension/src/LanguageServer/client.ts
@@ -1553,7 +1553,7 @@ export class DefaultClient implements Client {
             const params: QueryTranslationUnitSourceParams = {
                 uri: docUri.toString(),
                 ignoreExisting: !!replaceExisting,
-                workspaceFolderUri: this.RootPath
+                workspaceFolderUri: this.RootUri?.toString()
             };
             const response: QueryTranslationUnitSourceResult = await this.languageClient.sendRequest(QueryTranslationUnitSourceRequest, params);
             if (!response.candidates || response.candidates.length === 0) {
@@ -2230,7 +2230,7 @@ export class DefaultClient implements Client {
     public requestSwitchHeaderSource(rootPath: string, fileName: string): Thenable<string> {
         const params: SwitchHeaderSourceParams = {
             switchHeaderSourceFileName: fileName,
-            workspaceFolderUri: rootPath
+            workspaceFolderUri: this.RootUri?.toString()
         };
         return this.requestWhenReady(() => this.languageClient.sendRequest(SwitchHeaderSourceRequest, params));
     }
@@ -2376,7 +2376,7 @@ export class DefaultClient implements Client {
         const params: CppPropertiesParams = {
             configurations: [],
             currentConfiguration: this.configuration.CurrentConfigurationIndex,
-            workspaceFolderUri: this.RootPath,
+            workspaceFolderUri: this.RootUri?.toString(),
             isReady: true
         };
         const settings: CppSettings = new CppSettings(this.RootUri);
@@ -2429,7 +2429,7 @@ export class DefaultClient implements Client {
     private onSelectedConfigurationChanged(index: number): void {
         const params: FolderSelectedSettingParams = {
             currentConfiguration: index,
-            workspaceFolderUri: this.RootPath
+            workspaceFolderUri: this.RootUri?.toString(),
         };
         this.notifyWhenLanguageClientReady(() => {
             this.languageClient.sendNotification(ChangeSelectedSettingNotification, params);
@@ -2445,7 +2445,7 @@ export class DefaultClient implements Client {
     private onCompileCommandsChanged(path: string): void {
         const params: FileChangedParams = {
             uri: vscode.Uri.file(path).toString(),
-            workspaceFolderUri: this.RootPath
+            workspaceFolderUri: this.RootUri?.toString()
         };
         this.notifyWhenLanguageClientReady(() => this.languageClient.sendNotification(ChangeCompileCommandsNotification, params));
     }
@@ -2520,7 +2520,7 @@ export class DefaultClient implements Client {
 
         const params: CustomConfigurationParams = {
             configurationItems: sanitized,
-            workspaceFolderUri: this.RootPath
+            workspaceFolderUri: this.RootUri?.toString()
         };
 
         this.languageClient.sendNotification(CustomConfigurationNotification, params);
@@ -2611,7 +2611,7 @@ export class DefaultClient implements Client {
 
         const params: CustomBrowseConfigurationParams = {
             browseConfiguration: sanitized,
-            workspaceFolderUri: this.RootPath
+            workspaceFolderUri: this.RootUri?.toString()
         };
 
         this.languageClient.sendNotification(CustomBrowseConfigurationNotification, params);
@@ -2620,7 +2620,7 @@ export class DefaultClient implements Client {
     private clearCustomConfigurations(): void {
         this.configurationLogging.clear();
         const params: WorkspaceFolderParams = {
-            workspaceFolderUri: this.RootPath
+            workspaceFolderUri: this.RootUri?.toString()
         };
         this.notifyWhenLanguageClientReady(() => this.languageClient.sendNotification(ClearCustomConfigurationsNotification, params));
     }
@@ -2628,7 +2628,7 @@ export class DefaultClient implements Client {
     private clearCustomBrowseConfiguration(): void {
         this.browseConfigurationLogging = "";
         const params: WorkspaceFolderParams = {
-            workspaceFolderUri: this.RootPath
+            workspaceFolderUri: this.RootUri?.toString()
         };
         this.notifyWhenLanguageClientReady(() => this.languageClient.sendNotification(ClearCustomBrowseConfigurationNotification, params));
     }
