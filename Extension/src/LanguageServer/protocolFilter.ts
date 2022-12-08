9d3257611ca42a5a9085ea21be17a4fe8a8575f8
Colen Garoutte-Carson <coleng@microsoft.com>
2022-12-08 13:27:11 -0800

    Remove deferring from protocolFilter

--- Extension/src/LanguageServer/protocolFilter.ts
+++ Extension/src/LanguageServer/protocolFilter.ts
@@ -13,12 +13,10 @@ import { clients, onDidChangeActiveTextEditor } from './extension';
 
 export function createProtocolFilter(): Middleware {
     // Disabling lint for invoke handlers
-    const defaultHandler: (data: any, callback: (data: any) => Promise<void>) => Promise<void> = async (data, callback: (data: any) => void) => { clients.ActiveClient.notifyWhenLanguageClientReady(() => callback(data)); };
-    // const invoke1 = async (a: any, next: (a: any) => any) => { await clients.ActiveClient.awaitUntilLanguageClientReady(); return next(a); };
-    const invoke2 = async (a: any, b: any, next: (a: any, b: any) => any) => { await clients.ActiveClient.awaitUntilLanguageClientReady(); return next(a, b); };
-    const invoke3 = async (a: any, b: any, c: any, next: (a: any, b: any, c: any) => any) => { await clients.ActiveClient.awaitUntilLanguageClientReady(); return next(a, b, c); };
-    const invoke4 = async (a: any, b: any, c: any, d: any, next: (a: any, b: any, c: any, d: any) => any) => { await clients.ActiveClient.awaitUntilLanguageClientReady(); return next(a, b, c, d); };
-    // const invoke5 = async (a: any, b: any, c: any, d: any, e: any, next: (a: any, b: any, c: any, d: any, e: any) => any) => { await clients.ActiveClient.awaitUntilLanguageClientReady(); return next(a, b, c, d, e); };
+    const invoke1 = async (a: any, next: (a: any) => any) => next(a);
+    const invoke2 = async (a: any, b: any, next: (a: any, b: any) => any) => next(a, b);
+    const invoke3 = async (a: any, b: any, c: any, next: (a: any, b: any, c: any) => any) => next(a, b, c);
+    const invoke4 = async (a: any, b: any, c: any, d: any, next: (a: any, b: any, c: any, d: any) => any) => next(a, b, c, d);
     /* tslint:enable */
 
     return {
@@ -34,9 +32,9 @@ export function createProtocolFilter(): Middleware {
                     clients.timeTelemetryCollector.setDidOpenTime(document.uri);
                     if (clients.checkOwnership(me, document)) {
                         me.TrackedDocuments.add(document);
-                        const finishDidOpen = (doc: vscode.TextDocument) => {
-                            me.provideCustomConfiguration(doc.uri, undefined);
-                            sendMessage(doc);
+                        const finishDidOpen = async (doc: vscode.TextDocument) => {
+                            await me.provideCustomConfiguration(doc.uri, undefined);
+                            await sendMessage(doc);
                             me.onDidOpenTextDocument(doc);
                             if (editor && editor === vscode.window.activeTextEditor) {
                                 onDidChangeActiveTextEditor(editor);
@@ -50,14 +48,13 @@ export function createProtocolFilter(): Middleware {
                                 const mappingString: string = fileName + "@" + document.uri.fsPath;
                                 me.addFileAssociations(mappingString, "cpp");
                                 me.sendDidChangeSettings();
-                                vscode.languages.setTextDocumentLanguage(document, "cpp").then((newDoc: vscode.TextDocument) => {
-                                    finishDidOpen(newDoc);
-                                });
+                                const newDoc: vscode.TextDocument = await vscode.languages.setTextDocumentLanguage(document, "cpp");
+                                await finishDidOpen(newDoc);
                                 languageChanged = true;
                             }
                         }
                         if (!languageChanged) {
-                            finishDidOpen(document);
+                            await finishDidOpen(document);
                         }
                     }
                 }
@@ -74,45 +71,35 @@ export function createProtocolFilter(): Middleware {
         didChange: async (textDocumentChangeEvent, sendMessage) => {
             const me: Client = clients.getClientFor(textDocumentChangeEvent.document.uri);
             me.onDidChangeTextDocument(textDocumentChangeEvent);
-            sendMessage(textDocumentChangeEvent);
+            await sendMessage(textDocumentChangeEvent);
         },
-        willSave: defaultHandler,
+        willSave: invoke1,
         willSaveWaitUntil: async (event, sendMessage) => {
             const me: Client = clients.getClientFor(event.document.uri);
             if (me.TrackedDocuments.has(event.document)) {
-                // Don't use me.requestWhenReady or notifyWhenLanguageClientReady;
-                // otherwise, the message can be delayed too long.
                 return sendMessage(event);
             }
-            return Promise.resolve([]);
+            return [];
         },
-        didSave: defaultHandler,
+        didSave: invoke1,
         didClose: async (document, sendMessage) => {
             const me: Client = clients.getClientFor(document.uri);
             if (me.TrackedDocuments.has(document)) {
                 me.onDidCloseTextDocument(document);
                 me.TrackedDocuments.delete(document);
-                sendMessage(document);
+                await sendMessage(document);
             }
         },
-
         provideCompletionItem: invoke4,
         resolveCompletionItem: invoke2,
-        provideHover: (document, position, token, next: (document: any, position: any, token: any) => any) => {
-            const me: Client = clients.getClientFor(document.uri);
-            if (clients.checkOwnership(me, document)) {
-                return clients.ActiveClient.requestWhenReady(() => next(document, position, token));
-            }
-            return null;
-        },
+        provideHover: invoke3,
         provideSignatureHelp: invoke4,
         provideDefinition: invoke3,
         provideReferences: invoke4,
         provideDocumentHighlights: invoke3,
-        provideDeclaration: invoke3
-        // I believe the default handler will do the same thing.
-        // workspace: {
-        //     didChangeConfiguration: (sections, sendMessage) => sendMessage(sections)
-        // }
+        provideDeclaration: invoke3,
+        workspace: {
+            didChangeConfiguration: invoke1
+        }
     };
 }
