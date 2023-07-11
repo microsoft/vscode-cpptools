61d82dd3538d2c9d741eaae09cc9093360de8082
Sean McManus <seanmcm@microsoft.com>
2023-07-11 12:22:24 -0700

    Fix minimatch.

--- Extension/src/LanguageServer/client.ts
+++ Extension/src/LanguageServer/client.ts
@@ -24,6 +24,7 @@ import { WorkspaceSymbolProvider } from './Providers/workspaceSymbolProvider';
 
 import { fail } from 'assert';
 import * as fs from 'fs';
+import { minimatch } from "minimatch";
 import * as os from 'os';
 import { SourceFileConfiguration, SourceFileConfigurationItem, Version, WorkspaceBrowseConfiguration } from 'vscode-cpptools';
 import { IntelliSenseStatus, Status } from 'vscode-cpptools/out/testApi';
@@ -56,7 +57,6 @@ import { CppSettings, OtherSettings, SettingsParams, WorkspaceFolderSettingsPara
 import { SettingsTracker } from './settingsTracker';
 import { ConfigurationType, LanguageStatusUI, getUI } from './ui';
 import { handleChangedFromCppToC, makeVscodeLocation, makeVscodeRange } from './utils';
-import minimatch = require("minimatch");
 
 function deepCopy(obj: any) {
     return JSON.parse(JSON.stringify(obj));
