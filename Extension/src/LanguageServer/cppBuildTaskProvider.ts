1f4bbb53763cd7d7d630d783bee9bdedfc6f09f0
Tarik Brown <browntarik@microsoft.com>
2023-11-22 14:46:47 -0800

    Add shell quoting for command line arguments

--- Extension/src/LanguageServer/cppBuildTaskProvider.ts
+++ Extension/src/LanguageServer/cppBuildTaskProvider.ts
@@ -6,6 +6,7 @@
 import * as cp from "child_process";
 import * as os from 'os';
 import * as path from 'path';
+import * as parse from "shell-quote";
 import { CustomExecution, Disposable, Event, EventEmitter, ProcessExecution, Pseudoterminal, ShellExecution, Task, TaskDefinition, TaskEndEvent, TaskExecution, TaskGroup, TaskProvider, tasks, TaskScope, TerminalDimensions, TextEditor, window, workspace, WorkspaceFolder } from 'vscode';
 import * as nls from 'vscode-nls';
 import * as util from '../common';
@@ -435,6 +436,10 @@ class CustomBuildTaskTerminal implements Pseudoterminal {
 
         let child: cp.ChildProcess | undefined;
         try {
+            // Add shell quoting for command line arguments.
+            for (let i = 0; i < this.args.length; i++) {
+                this.args[i] = parse.quote([this.args[i]]);
+            }
             child = cp.spawn(command, this.args, this.options ? this.options : {});
             let error: string = "";
             let stdout: string = "";
