cf6b06b3330cd9fc4340f5e1c1811ab6e370abd8
Colen Garoutte-Carson <coleng@microsoft.com>
2024-09-26 16:13:10 -0700

    Fix an issue preventing use of a full command line in compilerPath

--- Extension/src/LanguageServer/configurations.ts
+++ Extension/src/LanguageServer/configurations.ts
@@ -594,7 +594,7 @@ export class CppProperties {
             configuration.intelliSenseMode === "${default}") {
             return "";
         }
-        const resolvedCompilerPath: string = this.resolvePath(configuration.compilerPath);
+        const resolvedCompilerPath: string = this.resolvePath(configuration.compilerPath, false, false);
         const settings: CppSettings = new CppSettings(this.rootUri);
         const compilerPathAndArgs: util.CompilerPathAndArgs = util.extractCompilerPathAndArgs(!!settings.legacyCompilerArgsBehavior, resolvedCompilerPath);
 
@@ -1878,7 +1878,7 @@ export class CppProperties {
         const paths: string[] = [];
         let compilerPath: string | undefined;
         for (const pathArray of [currentConfiguration.browse ? currentConfiguration.browse.path : undefined,
-            currentConfiguration.includePath, currentConfiguration.macFrameworkPath]) {
+        currentConfiguration.includePath, currentConfiguration.macFrameworkPath]) {
             if (pathArray) {
                 for (const curPath of pathArray) {
                     paths.push(`${curPath}`);
