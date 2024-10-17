42cd504fb469f91ba3a487b47584114e9ee924fd
Glen Chung <kuchung@microsoft.com>
2024-10-16 17:23:16 -0700

    Support A/B Compiler Arguments Traits
    - Depends on cpptools' update to provide ProjectContextResult.
    - Send "standardVersion" trait in completion prompt by default.
    - Added the following new traits
      - intelliSenseDisclaimer: compiler information disclaimer.
      - intelliSenseDisclaimerBeginning: to note the beginning of IntelliSense information.
      - compilerArguments: a list of compiler command arguments that could affect Copilot generating completions.
      - directAsks: direct asking Copilot to do something instead of providing an argument.
      - intelliSenseDisclaimerEnd: to note the end of IntelliSense information.
    - A/B Experimental flags
      - copilotcppTraits: deprecated, no longer used.
      - copilotcppExcludeTraits:: deprecated, no longer used.
      - copilotcppIncludeTraits: string array to include individual trait, i.e., compilerArguments.
      - copilotcppMsvcCompilerArgumentFilter: map of regex string to absence prompt for MSVC.
      - copilotcppClangCompilerArgumentFilter: map of regex string to absence prompt for Clang.
      - copilotcppGccCompilerArgumentFilter: map of regex string to absence prompt for GCC.
      - copilotcppCompilerArgumentDirectAskMap: map of argument to prompt.

--- Extension/src/LanguageServer/copilotProviders.ts
+++ Extension/src/LanguageServer/copilotProviders.ts
@@ -5,9 +5,13 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import { localize } from 'vscode-nls';
 import * as util from '../common';
-import { ChatContextResult, GetIncludesResult } from './client';
+import * as logger from '../logger';
+import * as telemetry from '../telemetry';
+import { GetIncludesResult } from './client';
 import { getActiveClient } from './extension';
+import { getCompilerArgumentFilterMap, getProjectContext } from './lmTool';
 
 export interface CopilotTrait {
     name: string;
@@ -34,35 +38,113 @@ export async function registerRelatedFilesProvider(): Promise<void> {
             for (const languageId of ['c', 'cpp', 'cuda-cpp']) {
                 api.registerRelatedFilesProvider(
                     { extensionId: util.extensionContext.extension.id, languageId },
-                    async (_uri: vscode.Uri, context: { flags: Record<string, unknown> }, token: vscode.CancellationToken) => {
+                    async (uri: vscode.Uri, context: { flags: Record<string, unknown> }, token: vscode.CancellationToken) => {
+                        const start = performance.now();
+                        const telemetryProperties: Record<string, string> = {};
+                        const telemetryMetrics: Record<string, number> = {};
+                        try {
+                            const getIncludesHandler = async () => (await getIncludesWithCancellation(1, token))?.includedFiles.map(file => vscode.Uri.file(file)) ?? [];
+                            const getTraitsHandler = async () => {
+                                const projectContext = await getProjectContext(uri, context, token);
 
-                        const getIncludesHandler = async () => (await getIncludesWithCancellation(1, token))?.includedFiles.map(file => vscode.Uri.file(file)) ?? [];
-                        const getTraitsHandler = async () => {
-                            const chatContext: ChatContextResult | undefined = await (getActiveClient().getChatContext(token) ?? undefined);
+                                if (!projectContext) {
+                                    return undefined;
+                                }
 
-                            if (!chatContext) {
-                                return undefined;
+                                let traits: CopilotTrait[] = [
+                                    { name: "intelliSenseDisclaimer", value: '', includeInPrompt: true, promptTextOverride: `IntelliSense is currently configured with the following compiler information. It reflects the active configuration, and the project may have more configurations targeting different platforms.` },
+                                    { name: "intelliSenseDisclaimerBeginning", value: '', includeInPrompt: true, promptTextOverride: `Beginning of IntelliSense information.` }
+                                ];
+                                if (projectContext.language) {
+                                    traits.push({ name: "language", value: projectContext.language, includeInPrompt: true, promptTextOverride: `The language is ${projectContext.language}.` });
+                                }
+                                if (projectContext.compiler) {
+                                    traits.push({ name: "compiler", value: projectContext.compiler, includeInPrompt: true, promptTextOverride: `This project compiles using ${projectContext.compiler}.` });
+                                }
+                                if (projectContext.standardVersion) {
+                                    traits.push({ name: "standardVersion", value: projectContext.standardVersion, includeInPrompt: true, promptTextOverride: `This project uses the ${projectContext.standardVersion} language standard.` });
+                                }
+                                if (projectContext.targetPlatform) {
+                                    traits.push({ name: "targetPlatform", value: projectContext.targetPlatform, includeInPrompt: true, promptTextOverride: `This build targets ${projectContext.targetPlatform}.` });
+                                }
+                                if (projectContext.targetArchitecture) {
+                                    traits.push({ name: "targetArchitecture", value: projectContext.targetArchitecture, includeInPrompt: true, promptTextOverride: `This build targets ${projectContext.targetArchitecture}.` });
+                                }
+
+                                if (projectContext.compiler) {
+                                    // We will process compiler arguments based on copilotcppXXXCompilerArgumentFilters and copilotcppCompilerArgumentDirectAskMap feature flags.
+                                    // The copilotcppXXXCompilerArgumentFilters are maps. The keys are regex strings for filtering and the values, if not empty,
+                                    // are the prompt text to use when no arguments are found.
+                                    // copilotcppCompilerArgumentDirectAskMap map individual matched argument to a prompt text.
+                                    // For duplicate matches, the last one will be used.
+                                    const filterMap = getCompilerArgumentFilterMap(projectContext.compiler, context);
+                                    if (filterMap !== undefined) {
+                                        const directAskMap: Record<string, string> = context.flags.copilotcppCompilerArgumentDirectAskMap ? JSON.parse(context.flags.copilotcppCompilerArgumentDirectAskMap as string) : {};
+                                        let directAsks: string = '';
+                                        const remainingArguments: string[] = [];
+
+                                        for (const key in filterMap) {
+                                            if (!key) {
+                                                continue;
+                                            }
+
+                                            const matchedArgument = projectContext.compilerArguments[key] as string;
+                                            if (matchedArgument?.length > 0) {
+                                                if (directAskMap[matchedArgument]) {
+                                                    directAsks += `${directAskMap[matchedArgument]} `;
+                                                } else {
+                                                    remainingArguments.push(matchedArgument);
+                                                }
+                                            } else if (filterMap[key]) {
+                                                // Use the prompt text in the absence of argument.
+                                                directAsks += `${filterMap[key]} `;
+                                            }
+                                        }
+
+                                        if (remainingArguments.length > 0) {
+                                            const compilerArgumentsValue = remainingArguments.join(", ");
+                                            traits.push({ name: "compilerArguments", value: compilerArgumentsValue, includeInPrompt: true, promptTextOverride: `The compiler arguments include: ${compilerArgumentsValue}.` });
+                                        }
+
+                                        if (directAsks) {
+                                            traits.push({ name: "directAsks", value: directAsks, includeInPrompt: true, promptTextOverride: directAsks });
+                                        }
+                                    }
+                                }
+
+                                traits.push({ name: "intelliSenseDisclaimerEnd", value: '', includeInPrompt: true, promptTextOverride: `End of IntelliSense information.` });
+
+                                const includeTraitsArray = context.flags.copilotcppIncludeTraits ? context.flags.copilotcppIncludeTraits as string[] : [];
+                                const includeTraits = new Set(includeTraitsArray);
+                                telemetryProperties["includeTraits"] = includeTraitsArray.join(',');
+
+                                // standardVersion trait is enabled by default.
+                                traits = traits.filter(trait => includeTraits.has(trait.name) || trait.name === 'standardVersion');
+
+                                telemetryProperties["traits"] = traits.map(trait => trait.name).join(',');
+                                return traits.length > 0 ? traits : undefined;
+                            };
+
+                            // Call both handlers in parallel
+                            const traitsPromise = getTraitsHandler();
+                            const includesPromise = getIncludesHandler();
+
+                            return { entries: await includesPromise, traits: await traitsPromise };
+                        }
+                        catch (exception) {
+                            try {
+                                const err: Error = exception as Error;
+                                logger.getOutputChannelLogger().appendLine(localize("copilot.relatedfilesprovider.error", "Error while retrieving result. Reason: {0}", err.message));
                             }
-
-                            let traits: CopilotTrait[] = [
-                                { name: "language", value: chatContext.language, includeInPrompt: true, promptTextOverride: `The language is ${chatContext.language}.` },
-                                { name: "compiler", value: chatContext.compiler, includeInPrompt: true, promptTextOverride: `This project compiles using ${chatContext.compiler}.` },
-                                { name: "standardVersion", value: chatContext.standardVersion, includeInPrompt: true, promptTextOverride: `This project uses the ${chatContext.standardVersion} language standard.` },
-                                { name: "targetPlatform", value: chatContext.targetPlatform, includeInPrompt: true, promptTextOverride: `This build targets ${chatContext.targetPlatform}.` },
-                                { name: "targetArchitecture", value: chatContext.targetArchitecture, includeInPrompt: true, promptTextOverride: `This build targets ${chatContext.targetArchitecture}.` }
-                            ];
-
-                            const excludeTraits = context.flags.copilotcppExcludeTraits as string[] ?? [];
-                            traits = traits.filter(trait => !excludeTraits.includes(trait.name));
-
-                            return traits.length > 0 ? traits : undefined;
-                        };
-
-                        // Call both handlers in parallel
-                        const traitsPromise = ((context.flags.copilotcppTraits as boolean) ?? false) ? getTraitsHandler() : Promise.resolve(undefined);
-                        const includesPromise = getIncludesHandler();
-
-                        return { entries: await includesPromise, traits: await traitsPromise };
+                            catch {
+                                // Intentionally swallow any exception.
+                            }
+                            telemetryProperties["error"] = "true";
+                            throw exception; // Throw the exception for auto-retry.
+                        } finally {
+                            telemetryMetrics['duration'] = performance.now() - start;
+                            telemetry.logCopilotEvent('RelatedFilesProvider', telemetryProperties, telemetryMetrics);
+                        }
                     }
                 );
             }
--- Extension/src/LanguageServer/lmTool.ts
+++ Extension/src/LanguageServer/lmTool.ts
@@ -9,9 +9,13 @@ import { localize } from 'vscode-nls';
 import * as util from '../common';
 import * as logger from '../logger';
 import * as telemetry from '../telemetry';
-import { ChatContextResult } from './client';
+import { ChatContextResult, ProjectContextResult } from './client';
 import { getClients } from './extension';
+import { checkDuration } from './utils';
 
+const MSVC: string = 'MSVC';
+const Clang: string = 'Clang';
+const GCC: string = 'GCC';
 const knownValues: { [Property in keyof ChatContextResult]?: { [id: string]: string } } = {
     language: {
         'c': 'C',
@@ -19,9 +23,9 @@ const knownValues: { [Property in keyof ChatContextResult]?: { [id: string]: str
         'cuda-cpp': 'CUDA C++'
     },
     compiler: {
-        'msvc': 'MSVC',
-        'clang': 'Clang',
-        'gcc': 'GCC'
+        'msvc': MSVC,
+        'clang': Clang,
+        'gcc': GCC
     },
     standardVersion: {
         'c++98': 'C++98',
@@ -44,6 +48,141 @@ const knownValues: { [Property in keyof ChatContextResult]?: { [id: string]: str
     }
 };
 
+function formatChatContext(context: ChatContextResult | ProjectContextResult): void {
+    type KnownKeys = 'language' | 'standardVersion' | 'compiler' | 'targetPlatform';
+    for (const key in knownValues) {
+        const knownKey = key as KnownKeys;
+        if (knownValues[knownKey] && context[knownKey]) {
+            // Clear the value if it's not in the known values.
+            context[knownKey] = knownValues[knownKey][context[knownKey]] || "";
+        }
+    }
+}
+
+export interface ProjectContext {
+    language: string;
+    standardVersion: string;
+    compiler: string;
+    targetPlatform: string;
+    targetArchitecture: string;
+    compilerArguments: Record<string, string>;
+}
+
+export function getCompilerArgumentFilterMap(compiler: string, context: { flags: Record<string, unknown> }): Record<string, string> | undefined {
+    // The copilotcppXXXCompilerArgumentFilters are maps.
+    // The keys are regex strings and the values, if not empty, are the prompt text to use when no arguments are found.
+    let filterMap: Record<string, string> | undefined;
+    try {
+        switch (compiler) {
+            case MSVC:
+                if (context.flags.copilotcppMsvcCompilerArgumentFilter !== undefined) {
+                    filterMap = JSON.parse(context.flags.copilotcppMsvcCompilerArgumentFilter as string);
+                }
+                break;
+            case Clang:
+                if (context.flags.copilotcppClangCompilerArgumentFilter !== undefined) {
+                    filterMap = JSON.parse(context.flags.copilotcppClangCompilerArgumentFilter as string);
+                }
+                break;
+            case GCC:
+                if (context.flags.copilotcppGccCompilerArgumentFilter !== undefined) {
+                    filterMap = JSON.parse(context.flags.copilotcppGccCompilerArgumentFilter as string);
+                }
+                break;
+        }
+    }
+    catch {
+        // Intentionally swallow any exception.
+    }
+    return filterMap;
+}
+
+function filterCompilerArguments(compiler: string, compilerArguments: string[], context: { flags: Record<string, unknown> }, telemetryProperties: Record<string, string>): Record<string, string> {
+    const filterMap = getCompilerArgumentFilterMap(compiler, context);
+    if (filterMap === undefined) {
+        return {};
+    }
+
+    const combinedArguments = compilerArguments.join(' ');
+    const result: Record<string, string> = {};
+    const filteredCompilerArguments: string[] = [];
+    for (const key in filterMap) {
+        if (!key) {
+            continue;
+        }
+        const filter = new RegExp(key, 'g');
+        const filtered = combinedArguments.match(filter);
+        if (filtered) {
+            filteredCompilerArguments.push(...filtered);
+            result[key] = filtered[filtered.length - 1];
+        }
+    }
+
+    if (filteredCompilerArguments.length > 0) {
+        // Telemetry to learn about the argument distribution. The filtered arguments are expected to be non-PII.
+        telemetryProperties["filteredCompilerArguments"] = filteredCompilerArguments.join(',');
+        telemetryProperties["filters"] = Object.keys(filterMap).filter(filter => !!filter).join(',');
+    }
+
+    return result;
+}
+
+export async function getProjectContext(uri: vscode.Uri, context: { flags: Record<string, unknown> }, token: vscode.CancellationToken): Promise<ProjectContext | undefined> {
+    const telemetryProperties: Record<string, string> = {};
+    const telemetryMetrics: Record<string, number> = {};
+    try {
+        const projectContext = await checkDuration<ProjectContextResult | undefined>(async () => await getClients()?.ActiveClient?.getProjectContext(uri, token) ?? undefined);
+        telemetryMetrics["duration"] = projectContext.duration;
+        if (!projectContext.result) {
+            return undefined;
+        }
+
+        formatChatContext(projectContext.result);
+
+        const result: ProjectContext = {
+            language: projectContext.result.language,
+            standardVersion: projectContext.result.standardVersion,
+            compiler: projectContext.result.compiler,
+            targetPlatform: projectContext.result.targetPlatform,
+            targetArchitecture: projectContext.result.targetArchitecture,
+            compilerArguments: {}
+        };
+
+        if (projectContext.result.language) {
+            telemetryProperties["language"] = projectContext.result.language;
+        }
+        if (projectContext.result.compiler) {
+            telemetryProperties["compiler"] = projectContext.result.compiler;
+        }
+        if (projectContext.result.standardVersion) {
+            telemetryProperties["standardVersion"] = projectContext.result.standardVersion;
+        }
+        if (projectContext.result.targetPlatform) {
+            telemetryProperties["targetPlatform"] = projectContext.result.targetPlatform;
+        }
+        if (projectContext.result.targetArchitecture) {
+            telemetryProperties["targetArchitecture"] = projectContext.result.targetArchitecture;
+        }
+        telemetryMetrics["compilerArgumentCount"] = projectContext.result.fileContext.compilerArguments.length;
+        result.compilerArguments = filterCompilerArguments(projectContext.result.compiler, projectContext.result.fileContext.compilerArguments, context, telemetryProperties);
+
+        return result;
+    }
+    catch (exception) {
+        try {
+            const err: Error = exception as Error;
+            logger.getOutputChannelLogger().appendLine(localize("copilot.projectcontext.error", "Error while retrieving the project context. Reason: {0}", err.message));
+        }
+        catch {
+            // Intentionally swallow any exception.
+        }
+        telemetryProperties["error"] = "true";
+        return undefined;
+    } finally {
+        telemetry.logCopilotEvent('ProjectContext', telemetryProperties, telemetryMetrics);
+    }
+}
+
 export class CppConfigurationLanguageModelTool implements vscode.LanguageModelTool<void> {
     public async invoke(options: vscode.LanguageModelToolInvocationOptions<void>, token: vscode.CancellationToken): Promise<vscode.LanguageModelToolResult> {
         return new vscode.LanguageModelToolResult([
@@ -58,18 +197,12 @@ export class CppConfigurationLanguageModelTool implements vscode.LanguageModelTo
                 return 'The active document is not a C, C++, or CUDA file.';
             }
 
-            const chatContext: ChatContextResult | undefined = await (getClients()?.ActiveClient?.getChatContext(token) ?? undefined);
+            const chatContext: ChatContextResult | undefined = await (getClients()?.ActiveClient?.getChatContext(currentDoc.uri, token) ?? undefined);
             if (!chatContext) {
                 return 'No configuration information is available for the active document.';
             }
 
-            for (const key in knownValues) {
-                const knownKey = key as keyof ChatContextResult;
-                if (knownValues[knownKey] && chatContext[knownKey]) {
-                    // Clear the value if it's not in the known values.
-                    chatContext[knownKey] = knownValues[knownKey][chatContext[knownKey]] || "";
-                }
-            }
+            formatChatContext(chatContext);
 
             let contextString = "";
             if (chatContext.language) {
@@ -100,7 +233,7 @@ export class CppConfigurationLanguageModelTool implements vscode.LanguageModelTo
             telemetryProperties["error"] = "true";
             return "";
         } finally {
-            telemetry.logLanguageModelToolEvent('cpp', telemetryProperties);
+            telemetry.logCopilotEvent('Chat/Tool/cpp', telemetryProperties);
         }
     }
 
