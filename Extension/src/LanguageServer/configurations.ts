e79c6b24391022d5a82a25a94f5351d30400ac3d
yiftahw <63462505+yiftahw@users.noreply.github.com>
2024-11-09 22:27:50 +0200

    keep track of fallback time per file

--- Extension/src/LanguageServer/configurations.ts
+++ Extension/src/LanguageServer/configurations.ts
@@ -138,7 +138,7 @@ export class CppProperties {
     private configFileWatcherFallbackTime: Date = new Date(); // Used when file watching fails.
     private compileCommandsFile: vscode.Uri | undefined | null = undefined;
     private compileCommandsFileWatchers: fs.FSWatcher[] = [];
-    private compileCommandsFileWatcherFallbackTime: Date = new Date(); // Used when file watching fails.
+    private compileCommandsFileWatcherFallbackTime: Map<string, Date> = new Map<string, Date>(); // Used when file watching fails.
     private defaultCompilerPath: string | null = null;
     private knownCompilers?: KnownCompiler[];
     private defaultCStandard: string | null = null;
@@ -1093,6 +1093,10 @@ export class CppProperties {
 
             if (configuration.compileCommands) {
                 configuration.compileCommands = this.resolvePath(configuration.compileCommands);
+                if (!this.compileCommandsFileWatcherFallbackTime.has(configuration.compileCommands)) {
+                    // Start tracking the fallback time for a new path.
+                    this.compileCommandsFileWatcherFallbackTime.set(configuration.compileCommands, new Date());
+                }
             }
 
             if (configuration.forcedInclude) {
@@ -1104,9 +1108,26 @@ export class CppProperties {
             }
         }
 
+        this.clearStaleCompileCommandsFileWatcherFallbackTimes();
         this.updateCompileCommandsFileWatchers();
         if (!this.configurationIncomplete) {
             this.onConfigurationsChanged();
+        }
+    }
+
+    private clearStaleCompileCommandsFileWatcherFallbackTimes(): void {
+        const trackedCompileCommandsPaths: Set<string> = new Set();
+        this.configurationJson?.configurations.forEach((config: Configuration) => {
+            const path = this.resolvePath(config.compileCommands);
+            if (path.length > 0) {
+                trackedCompileCommandsPaths.add(path);
+            }
+        });
+
+        for (const path of this.compileCommandsFileWatcherFallbackTime.keys()) {
+            if (!trackedCompileCommandsPaths.has(path)) {
+                this.compileCommandsFileWatcherFallbackTime.delete(path);
+            }
         }
     }
 
@@ -2308,6 +2329,7 @@ export class CppProperties {
             return;
         }
         const compileCommandsFile: string | undefined = this.resolvePath(compileCommands);
+        const compileCommandsLastChanged: Date | undefined = this.compileCommandsFileWatcherFallbackTime.get(compileCommandsFile);
         fs.stat(compileCommandsFile, (err, stats) => {
             if (err) {
                 if (err.code === "ENOENT" && this.compileCommandsFile) {
@@ -2316,8 +2338,8 @@ export class CppProperties {
                     this.onCompileCommandsChanged(compileCommandsFile);
                     this.compileCommandsFile = null; // File deleted
                 }
-            } else if (stats.mtime > this.compileCommandsFileWatcherFallbackTime) {
-                this.compileCommandsFileWatcherFallbackTime = new Date();
+            } else if (compileCommandsLastChanged !== undefined && stats.mtime > compileCommandsLastChanged) {
+                this.compileCommandsFileWatcherFallbackTime.set(compileCommandsFile, new Date());
                 this.onCompileCommandsChanged(compileCommandsFile);
                 this.compileCommandsFile = vscode.Uri.file(compileCommandsFile); // File created.
             }
