450d2144af5c0be2cc199f216049a8e7d262598f
Colen Garoutte-Carson <coleng@microsoft.com>
2024-12-19 15:14:54 -0800

    Address PR feedback

--- Extension/src/LanguageServer/client.ts
+++ Extension/src/LanguageServer/client.ts
@@ -1384,10 +1384,7 @@ export class DefaultClient implements Client {
         const filesEncoding: string = otherSettings.filesEncoding;
         let filesEncodingChanged: boolean = false;
         if (workspaceFolder) {
-            const lastFilesEncoding: PersistentFolderState<string> = new PersistentFolderState<string>("CPP.lastFilesEncoding", "", workspaceFolder);
-            if (lastFilesEncoding.Value === "") {
-                lastFilesEncoding.Value = filesEncoding;
-            }
+            const lastFilesEncoding: PersistentFolderState<string> = new PersistentFolderState<string>("CPP.lastFilesEncoding", filesEncoding, workspaceFolder);
             filesEncodingChanged = lastFilesEncoding.Value !== filesEncoding;
         }
         const result: WorkspaceFolderSettingsParams = {
@@ -1522,10 +1519,7 @@ export class DefaultClient implements Client {
             void util.promptForReloadWindowDueToSettingsChange();
         }
         const workspaceFallbackEncoding: string = workspaceOtherSettings.filesEncoding;
-        const lastWorkspaceFallbackEncoding: PersistentState<string> = new PersistentState<string>("CPP.lastWorkspaceFallbackEncoding", "");
-        if (lastWorkspaceFallbackEncoding.Value === "") {
-            lastWorkspaceFallbackEncoding.Value = workspaceFallbackEncoding;
-        }
+        const lastWorkspaceFallbackEncoding: PersistentState<string> = new PersistentState<string>("CPP.lastWorkspaceFallbackEncoding", workspaceFallbackEncoding);
         const workspaceFallbackEncodingChanged = lastWorkspaceFallbackEncoding.Value !== workspaceFallbackEncoding;
         return {
             filesAssociations: workspaceOtherSettings.filesAssociations,
