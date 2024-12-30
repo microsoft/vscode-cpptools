2bff033a27c9e6bd1ebefcf9b1ca531e68b6238e
Yiftah Waisman <63462505+yiftahw@users.noreply.github.com>
2024-12-30 20:31:13 +0200

    Handle multiple compile commands on client side (needs native server side support) (#12960)

--- Extension/src/LanguageServer/settings.ts
+++ Extension/src/LanguageServer/settings.ts
@@ -402,7 +402,17 @@ export class CppSettings extends Settings {
     public get defaultDotconfig(): string | undefined { return changeBlankStringToUndefined(this.getAsStringOrUndefined("default.dotConfig")); }
     public get defaultMacFrameworkPath(): string[] | undefined { return this.getArrayOfStringsWithUndefinedDefault("default.macFrameworkPath"); }
     public get defaultWindowsSdkVersion(): string | undefined { return changeBlankStringToUndefined(this.getAsStringOrUndefined("default.windowsSdkVersion")); }
-    public get defaultCompileCommands(): string | undefined { return changeBlankStringToUndefined(this.getAsStringOrUndefined("default.compileCommands")); }
+    public get defaultCompileCommands(): string[] | undefined {
+        const value: any = super.Section.get<any>("default.compileCommands");
+        if (isString(value)) {
+            return value.length > 0 ? [value] : undefined;
+        }
+        if (isArrayOfString(value)) {
+            const result = value.filter(x => x.length > 0);
+            return result.length > 0 ? result : undefined;
+        }
+        return undefined;
+    }
     public get defaultForcedInclude(): string[] | undefined { return this.getArrayOfStringsWithUndefinedDefault("default.forcedInclude"); }
     public get defaultIntelliSenseMode(): string | undefined { return this.getAsStringOrUndefined("default.intelliSenseMode"); }
     public get defaultCompilerPath(): string | null { return this.getAsString("default.compilerPath", true); }
@@ -652,7 +662,7 @@ export class CppSettings extends Settings {
         }
 
         if (isArrayOfString(value)) {
-            if (setting.items.enum && !allowUndefinedEnums) {
+            if (setting.items?.enum !== undefined && !allowUndefinedEnums) {
                 if (!value.every(x => this.isValidEnum(setting.items.enum, x))) {
                     return setting.default;
                 }
