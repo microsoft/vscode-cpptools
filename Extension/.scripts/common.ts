792d8f9801b7d43252f722dfaa9afa9228155918
spebl <spebl@microsoft.com>
2025-01-15 13:55:19 -0800

    update scripts to delete problematic development proposal

--- Extension/.scripts/common.ts
+++ Extension/.scripts/common.ts
@@ -48,7 +48,7 @@ export const Git = async (...args: Parameters<Awaited<CommandFunction>>) => (awa
 export const GitClean = async (...args: Parameters<Awaited<CommandFunction>>) => (await new Command(await git, 'clean'))(...args);
 
 export async function getModifiedIgnoredFiles() {
-    const {code, error, stdio } = await GitClean('-Xd', '-n');
+    const { code, error, stdio } = await GitClean('-Xd', '-n');
     if (code) {
         throw new Error(`\n${error.all().join('\n')}`);
     }
@@ -65,11 +65,11 @@ export async function rimraf(...paths: string[]) {
         }
         if (await filepath.isFolder(each)) {
             verbose(`Removing folder ${red(each)}`);
-            all.push(rm(each, {recursive: true, force: true}));
+            all.push(rm(each, { recursive: true, force: true }));
             continue;
         }
         verbose(`Removing file ${red(each)}`);
-        all.push(rm(each, {force: true}));
+        all.push(rm(each, { force: true }));
     }
     await Promise.all(all);
 }
@@ -342,6 +342,18 @@ export async function checkBinaries() {
 
     if (!failing) {
         verbose('Native binary files appear to be in place.');
+    }
+    return failing;
+}
+
+export async function checkProposals() {
+    let failing = false;
+
+    await rm(`${$root}/vscode.proposed.chatParticipantAdditions.d.ts`);
+    failing = await assertAnyFile('vscode.proposed.chatParticipantAdditions.d.ts') && (quiet || warn(`The VSCode import file '${$root}/vscode.proposed.chatParticipantAdditions.d.ts' should not be present.`)) || failing;
+
+    if (!failing) {
+        verbose('VSCode proposals appear to be in place.');
     }
     return failing;
 }
