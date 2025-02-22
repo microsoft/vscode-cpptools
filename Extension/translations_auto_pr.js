ab188891276a080299b102a7c050e8cc0273104a
Sean McManus <seanmcm@microsoft.com>
2025-02-21 17:28:18 -0800

    Try another fix.

--- Extension/translations_auto_pr.js
+++ Extension/translations_auto_pr.js
@@ -187,7 +187,7 @@ console.log(`pushing to remote branch (git push -f origin ${branchName})`);
 cp.execSync(`git push -f origin ${branchName}`);
 
 console.log("Checking if there is already a pull request...");
-const octokit = new Octokit.Octokit({auth: authToken});
+const octokit = new Octokit({auth: authToken});
 octokit.pulls.list({ owner: repoOwner, repo: repoName }).then(({data}) => {
     let alreadyHasPullRequest = false;
     if (data) {
