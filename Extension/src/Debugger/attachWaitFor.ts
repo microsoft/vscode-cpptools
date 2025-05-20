8fd5e03b6f84a0f3bbfd3921907f6353fb56707c
Borja Munoz Fernandez <borja.munoz@external.epiroc.com>
2025-05-20 16:59:46 +0200

    let's og

--- Extension/src/Debugger/attachWaitFor.ts
+++ Extension/src/Debugger/attachWaitFor.ts
@@ -1,84 +1,125 @@
 
+import * as os from 'os';
 import * as vscode from 'vscode';
 import * as util from '../common';
 import { sleep } from '../Utility/Async/sleep';
-import { PsProcessParser } from './nativeAttach';
+import { CimAttachItemsProvider, PsAttachItemsProvider, WmicAttachItemsProvider } from './nativeAttach';
 
-export class AttachWaitFor {
+export interface WaitForProcessProvider {
+    poll(program: string, timeout: number, interval: number, token?: vscode.CancellationToken): Promise<string | undefined>
+}
 
-    constructor() {
+export class PollProcessProviderFactory {
+    static Get(): WaitForProcessProvider {
+        if (os.platform() === 'win32') {
+            const pwsh: string | undefined = util.findPowerShell();
+            let itemsProvider = pwsh ? new CimAttachItemsProvider(pwsh) : new WmicAttachItemsProvider();
+            return new PollWindowsProvider(itemsProvider);
+        } else {
+            // Linux and MacOS
+            return new PollProcProvider(new PsAttachItemsProvider());
+        }
+    }
+}
+
+export class PollProcProvider implements WaitForProcessProvider {
+
+    constructor(itemsProvider: PsAttachItemsProvider) {
+        this.itemsProvider = itemsProvider;
         this._channel = vscode.window.createOutputChannel('waitfor-attach');
-        this.timeout = 30000
     }
 
     private _channel: vscode.OutputChannel;
+    private itemsProvider: PsAttachItemsProvider;
+
+    async poll(program: string, timeout: number, interval: number, token?: vscode.CancellationToken): Promise<string | undefined> {
+        return new Promise<string | undefined>(async (resolve, reject) => {
+            const startTime = Date.now();  // Get the current time in milliseconds
+            let process: string | undefined;
+            while (true) {
+                let elapsedTime = Date.now() - startTime;
+                if (elapsedTime >= timeout) {
+                    reject(new Error('Timeout reached. No process matched the pattern.'));
+                }
+
+                if (token?.isCancellationRequested) {
+                    reject(new Error('Operation cancelled.'));
+                }
+
+                let procs = await this.itemsProvider.getAttachItems(token)
+                for (const proc of procs) {
+                    if (proc.detail?.includes(program)) {
+                        process = proc.id
+                        break
+                    }
+                }
+
+                if (process) {
+                    await util.execChildProcess(`kill -STOP ${process}`, undefined, this._channel);
+                    resolve(process)
+                }
+
+                sleep(interval)
+            }
+        })
+    }
+}
+
+export class PollWindowsProvider implements WaitForProcessProvider {
+    constructor(itemsProvider: CimAttachItemsProvider | WmicAttachItemsProvider) {
+        this.itemsProvider = itemsProvider;
+    }
+
+    private itemsProvider: CimAttachItemsProvider | WmicAttachItemsProvider;
+
+    public async poll(program: string, timeout: number, interval: number, token?: vscode.CancellationToken): Promise<string | undefined> {
+        return new Promise<string | undefined>(async (resolve, reject) => {
+            const startTime = Date.now();  // Get the current time in milliseconds
+            let process: string | undefined;
+            while (true) {
+                const elapsedTime = Date.now() - startTime;
+                if (elapsedTime >= timeout) {
+                    reject(new Error('Timeout reached. No process matched the pattern.'));
+                }
+
+                // Check for cancellation
+                if (token?.isCancellationRequested) {
+                    reject(new Error('Operation cancelled.'));
+                }
+
+                let procs = await this.itemsProvider.getAttachItems(token)
+                for (const proc of procs) {
+                    if (proc.detail?.includes(program)) {
+                        process = proc.id
+                        break
+                    }
+                }
+
+                if (process) {
+                    // Use pssupend to send SIGSTOP analogous in Windows
+                    await util.execChildProcess(`pssuspend.exe /accepteula -nobanner ${process}`, undefined, undefined)
+                    resolve(process)
+                }
+
+                sleep(interval)
+            }
+        })
+    }
+}
+
+
+export class AttachWaitFor {
+    constructor(private poller: WaitForProcessProvider) {
+        //this._channel = vscode.window.createOutputChannel('waitfor-attach');
+        this.timeout = 30000
+    }
+
     private timeout: number;
 
-    public async WaitForProcess(program: string, timeout: number): Promise<string | undefined> {
+    public async WaitForProcess(program: string, timeout: number, interval: number, token?: vscode.CancellationToken): Promise<string | undefined> {
         if (timeout) {
             this.timeout = timeout
         }
-
-        return await this.poll(program)
+        return await this.poller.poll(program, this.timeout, interval, token);
     }
-
-    //Naive poll mechanism, parses /proc for a while till a match is found
-    private async poll(program: string): Promise<string | undefined> {
-        this._channel.clear()
-        const startTime = Date.now();  // Get the current time in milliseconds
-        let seen = new Set<string>();
-        let process: string | undefined;
-        while (true) {
-            const elapsedTime = Date.now() - startTime;
-
-            if (elapsedTime >= this.timeout) {
-                console.log('Timeout reached. No process matched pattern.');
-                return undefined
-            }
-
-            const output: string = await util.execChildProcess(PsProcessParser.psLinuxCommand, undefined, this._channel)
-            const lines: string[] = output.split(/\r?\n/);
-            const processes: string[] = lines.slice(1);
-            const processAttach = PsProcessParser.ParseProcessFromPsArray(processes)
-                .sort((a, b) => {
-                    if (a.name === undefined) {
-                        if (b.name === undefined) {
-                            return 0;
-                        }
-                        return 1;
-                    }
-                    if (b.name === undefined) {
-                        return -1;
-                    }
-                    const aLower: string = a.name.toLowerCase();
-                    const bLower: string = b.name.toLowerCase();
-                    if (aLower === bLower) {
-                        return 0;
-                    }
-                    return aLower < bLower ? -1 : 1;
-                })
-                .map(p => p.toAttachItem());
-            processAttach.forEach(p => {
-                if (!process && p.detail!.includes(program)) {
-                    console.log("Found program waiting for with pid %s - info %s", p.id!, p.detail!)
-                    process = p.id!
-
-                    // Send sigstop by default?
-                    util.execChildProcess(`kill -STOP ${process}`, undefined, this._channel)
-                    return
-                }
-
-                if (seen.has(p.id!) == false && p.label != "ps" && !p.detail!.includes("ps")) {
-                    seen.add(p.id!)
-                }
-            })
-
-            if (process) {
-                return process
-            }
-
-            sleep(200)
-        }
-    }
-
 }
