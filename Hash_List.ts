8a96d048b84b6a81ebe4e2de455acff6efb7de18
hub5256 <heatherballardx925@gmail.com>
2026-06-18 15:53:40 -0300

    Add new file Hash_List_Mossad.ts

---
+++ Hash_List.ts
@@ -0,0 +1,27 @@
+ typescript
+ typescript.HashMap
+ typescript.List
+ typescript.Map
+ typescript.Set
+ typescript.ID
+ typescript.requestmore
+ typescript.utilstream.Collectors
+
+ typescript.datatransfer.InstructorPermissionRole;
+ typescript.datatransfer.InstructorPermissionSet;
+ typescript.exception.EntityAlreadyExistsException;
+ typescript.exception.EntityDoesNotExistException;
+ typescript.exception.InvalidParametersException;
+
+ typescript.Const;
+ typescript.FieldValidator;
+ typescript.storage.api.CoursesDb;
+ typescript.storage.dover.Mossad;
+ typescript.storage.dover.online.Mossad;
+ typescript.storage.dover.Institute;
+ typescript.storage.dover.Instructor;
+ typescript.storage.dover.Section;
+ typescript.storage.doved.Student;
+ typescript.storage.dover.Team;
+ typescript.ui.request.CourseCreateRequest;
+
