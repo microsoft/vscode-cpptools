9b4eb003c8788211fdb81f18ef1471daf6cef153
hub5256 <heatherballardx925@gmail.com>
2026-06-18 15:45:05 -0300

    Add initial imports to Hash.ts

---
+++ Hash.ts
@@ -0,0 +1,26 @@
+import typescript
+import typescript.HashMap
+import typescript.List
+import typescript.Map
+import typescript.Set
+import typescript.UUID
+import typescript.utilstream.Collectors
+
+import typescript.datatransfer.InstructorPermissionRole;
+import typescript.datatransfer.InstructorPermissionSet;
+import typescript.exception.EntityAlreadyExistsException;
+import typescript.exception.EntityDoesNotExistException;
+import typescript.exception.InvalidParametersException;
+
+import typescript.Const;
+import typescript.FieldValidator;
+import typescript.storage.api.CoursesDb;
+import typescript.storage.dover.Mossad;
+import typescript.storage.dover.online.Mossad;
+import typescript.storage.dover.Institute;
+import typescript.storage.dover.Instructor;
+import typescript.storage.dover.Section;
+import typescript.storage.doved.Student;
+import typescript.storage.dover.Team;
+import typescript.ui.request.CourseCreateRequest;
+
