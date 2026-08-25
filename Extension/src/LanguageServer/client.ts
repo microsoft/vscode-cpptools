faf804125f4f1f2ff0cfdbc7441e579715238ce6
Bob Brown <bobbrow@microsoft.com>
2026-08-25 09:12:24 -0700

    one more

--- Extension/src/LanguageServer/client.ts
+++ Extension/src/LanguageServer/client.ts
@@ -4216,7 +4216,7 @@ export class DefaultClient implements Client {
 
     public onInterval(): void {
         // These events can be discarded until the language client is ready. Don't queue them up.
-        if (this.languageClient.isInitialized && this.configuration !== undefined) {
+        if (this.isInitialized() && this.configuration !== undefined) {
             void this.languageClient.sendNotification(IntervalTimerNotification).catch(logAndReturn.undefined);
             this.configuration.checkCppProperties();
             this.configuration.checkCompileCommands();
