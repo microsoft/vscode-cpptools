cc0f7e7f7c8cc8df6bc119228001e55cf3989244
Sean McManus <seanmcm@microsoft.com>
2026-09-17 17:23:24 -0700

    Fix Actions Octokit response types

--- .github/actions/common/OctokitTypings.ts
+++ .github/actions/common/OctokitTypings.ts
@@ -3,14 +3,18 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import type { RestEndpointMethodTypes } from '@octokit/rest';
+import type { getOctokit } from '@actions/github';
+
+type Rest = ReturnType<typeof getOctokit>['rest'];
 
 // This file is just to re-export some octokit types as nicer names
 
 export type ActionsListWorkflowRunsResponseWorkflowRunsItem =
-	RestEndpointMethodTypes['actions']['listWorkflowRuns']['response']['data']['workflow_runs'][0];
+	Awaited<ReturnType<Rest['actions']['listWorkflowRuns']>>['data']['workflow_runs'][0];
 
-export type IssueGetResponse = RestEndpointMethodTypes['issues']['get']['response']['data'];
+export type IssueGetResponse = Awaited<ReturnType<Rest['issues']['get']>>['data'];
 
-export type IssuesGetResponseMilestone =
-	RestEndpointMethodTypes['issues']['get']['response']['data']['milestone'];
+export type IssueSearchResult =
+	Awaited<ReturnType<Rest['search']['issuesAndPullRequests']>>['data']['items'][0];
+
+export type IssuesGetResponseMilestone = IssueGetResponse['milestone'];
