29220333b109d40b3aa83202ff01040305c7dfab
Colen Garoutte-Carson <49173979+Colengms@users.noreply.github.com>
2026-09-17 18:39:23 -0700

    Implement provideDocumentRangesFormattingEdits (#14776)

--- Extension/src/LanguageServer/client.ts
+++ Extension/src/LanguageServer/client.ts
@@ -351,6 +351,7 @@ export interface GetDocumentSymbolResult {
 }
 
 export interface FormatParams extends SelectionParams {
+    ranges?: Range[];
     character: string;
     insertSpaces: boolean;
     tabSize: number;
